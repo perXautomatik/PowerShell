57a2a9f710ebce152ca4b4bb2919c3d9f9082f66
perXautomatik <christoffer.broback@gmail.com>
2022-03-19 13:55:01 +0100

    [Chore] Reorganized

---
+++ actOnJson.ps1
@@ -0,0 +1,12 @@
+﻿cd (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+. .\getChildrenRecursive.ps1
+. .\presentJson.ps1
+
+$jsonx = "D:\OneDrive\TabSessionManager - Backup\tablerone_backup_with_thumbs_2021-04-19-09-33-16.txt"
+$jsonx = Get-Content $jsonx -Raw | ConvertFrom-Json
+
+$presentationMethodPath = (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -ChildPath "presentJson.ps1" )
+
+
+getChildrenRecursive $jsonx $presentationMethodPath
+
