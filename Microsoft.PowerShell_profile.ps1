f33d507296f5487654e8eb003ec96b129d47e22a
perXautomatik <christoffer.broback@gmail.com>
2022-04-12 10:58:50 +0200

    [feature] credit to kreloc [debug] reverted some pshell 7 semantics

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -63,12 +63,15 @@ $pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
 #https://www.sapien.com/blog/2014/10/21/a-better-tostring-method-for-hash-tables/
 
 #better hashtable ToString method
+if ( $(Test-CommandExists 'System.Collections.HashTable.ToString') ) {
+
   Update-TypeData -TypeName "System.Collections.HashTable"   `
   -MemberType ScriptMethod `
   -MemberName "ToString" -Value { $hashstr = "@{"; $keys = $this.keys; foreach ($key in $keys) { $v = $this[$key];
 	     if ($key -match "\s") { $hashstr += "`"$key`"" + "=" + "`"$v`"" + ";" }
 	     else { $hashstr += $key + "=" + "`"$v`"" + ";" } }; $hashstr += "}";
 	     return $hashstr }
+}
 #-------------------------------  overloading end
 
 #------------------------------- SystemMigration      -------------------------------
