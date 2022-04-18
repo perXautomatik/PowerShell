969b9cfbdf1886ddde8e0708b35f1214e626ed96
perXautomatik <christoffer.broback@gmail.com>
2022-04-18 23:26:02 +0200

    [feature] checking before trying to add

--- setPaths.ps1
+++ setPaths.ps1
@@ -19,36 +19,45 @@ $profileFolder = (split-path $profile -Parent)
 $EnvPath = join-path -Path $profileFolder -ChildPath 'Snipps'
 $env:Path += ";$EnvPath"
 
-$global:historyPath = "$home\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+$historyX = "$home\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+
+if( test-path $historyX) {$global:historyPath = $historyX} else {"$historyX not found"}
 
 #$path = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
 
 # vscode Portable Path
 $vscodepath = 'D:\portapps\6, Text,programming, x Editing\PortableApps\vscode-portable\vscode-portable.exe'
-[Environment]::SetEnvironmentVariable("code", $vscodepath)
+if( test-path $vscodepath) {[Environment]::SetEnvironmentVariable("code", $vscodepath)} else {"$vscodepath not found"}
+
 
 #sqlite dll
 $workpath = "C:\Users\crbk01\AppData\Local\GMap.NET\DllCache\SQLite_v103_NET4_x64\System.Data.SQLite.DLL"  ; 
+
+if (Test-ModuleExists 'pseverything') {
 $alternative = @(everything 'wfn:System.Data.SQLite.DLL')[0] ;
 $p = if(Test-Path $workpath){$workpath} else {$alternative} ;
 Add-Type -Path $p
+}
+else
+{"pseverything not loaded" ; "sqlite not loaded"}
+
+
+
 
 ### local variables
 $global:whatPulseDbQuery = "select rightstr(path,instr(reverse(path),'/')-1) exe,path from (select max(path) path,max(cast(replace(version,'.','') as integer)) version from applications group by case when online_app_id = 0 then name else online_app_id end)"
-$global:whatPulseDbPath = @(Everything 'whatpulse.db')[0]; 
+if (Test-ModuleExists 'pseverything') { $global:whatPulseDbPath = @(Everything 'whatpulse.db')[0];
 [Environment]::SetEnvironmentVariable("WHATPULSE_DB", $whatPulseDbPath)
 if (-not $env:WHATPULSE_DB) { $env:WHATPULSE_DB = $whatPulseDbPath }; $WHATPULSE_DB = $env:WHATPULSE_DB
-
+} else {"whatPulseDbpath not set"}
 
 [Environment]::SetEnvironmentVariable("WHATPULSE_QUERY", $whatPulseDbQuery)
 if (-not $env:WHATPULSE_QUERY) { $env:WHATPULSE_QUERY = $whatPulseDbQuery }; $WHATPULSE_QUERY = $env:WHATPULSE_QUERY
 
+$datagripx = '$home\appdata\Roaming\JetBrains\DataGrip2021.1'
+if (test-path $datagripx) { $global:datagripPath = $datagripx ; [Environment]::SetEnvironmentVariable("datagripPath", $datagripx) } else {"datagrippath not set"}
 
-
-
-$global:datagripPath = '$home\appdata\Roaming\JetBrains\DataGrip2021.1'
-[Environment]::SetEnvironmentVariable("datagripPath", $datagripPath)
-$global:bComparePath = 'D:\PortableApps\2. fileOrganization\PortableApps\Beyond Compare 4'
-[Environment]::SetEnvironmentVariable("bComparePath", $bComparePath)
-
+$bcompareX = 'D:\PortableApps\2. fileOrganization\PortableApps\Beyond Compare 4'
+if (test-path $bcompareX) { $global:bComparePath = $bcompareX ; [Environment]::SetEnvironmentVariable("bComparePath", $bcompareX)
+} else {"bcompare path not set"}
 echo "paths set"
