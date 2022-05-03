4197a80faf3b2ec06025d6c4de92e07bb58f7fd7
perXautomatik <christoffer.broback@gmail.com>
2022-05-03 14:56:03 +0200

    [feature] auto import custom modules

--- importModules.ps1
+++ importModules.ps1
@@ -140,23 +140,26 @@ function Import-MyModules {
 
 	    #-------------------------------  Set Hot-keys BEGIN  -------------------------------
     
-	    # Set-PSReadLineOption -EditMode Emac
-    
-	    # 每次回溯输入历史，光标定位于输入内容末尾
-	    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-
-	    # 设置 Tab 为菜单补全和 Intellisense
+        $PSReadLineOptions = @{
+            PredictionSource = "HistoryAndPlugin"
+            HistorySearchCursorMovesToEnd = $true                        
+        }
+        Set-PSReadLineOption @PSReadLineOptions
 	    
-	    Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
-    	    Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete       
-   	    # 设置 Ctrl+d 为退出 PowerShell
-	    Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
-
+        # Set-PSReadLineOption -EditMode Emac
+         
+	    # 每次回溯输入历史，光标定位于输入内容末尾    
+	    # 设置 Tab 为菜单补全和 Intellisense    
+        # 设置 Ctrl+d 为退出 PowerShell
 	    # 设置 Ctrl+z 为撤销
+	    # 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
+        Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete       
+
+        Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+	    Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
 	    Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
 
-	    # 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
-	    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+        Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 	    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 	    #-------------------------------  Set Hot-keys END    -------------------------------
