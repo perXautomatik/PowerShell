619f5a8e27fcd94405ca763502e26bfe44a51156
perXautomatik <christoffer.broback@gmail.com>
2022-06-28 15:08:22 +0200

    [feature] load segments as jobs

--- profile.ps1
+++ profile.ps1
@@ -1,71 +1,3 @@
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# src: https://gist.github.com/apfelchips/62a71500a0f044477698da71634ab87b
-# New-Item $(Split-Path "$($PROFILE.CurrentUserCurrentHost)") -ItemType Directory -ea 0; Invoke-WebRequest -Uri "https://git.io/JYZTu" -OutFile "$($PROFILE.CurrentUserCurrentHost)"
-
-# ref: https://devblogs.microsoft.com/powershell/optimizing-your-profile/#measure-script
-# ref: Powershell $? https://stackoverflow.com/a/55362991
-
-# ref: Write-* https://stackoverflow.com/a/38527767
-# Write-Host wrapper for Write-Information -InformationAction Continue
-# define these environment variables if not set already and also provide them as PSVariables
-
-if ( ( $null -eq $PSVersionTable.PSEdition) -or ($PSVersionTable.PSEdition -eq "Desktop") ) { $PSVersionTable.PSEdition = "Desktop" ;$IsWindows = $true }
-
-if ( -not $IsWindows ) { function Test-IsAdmin { if ( (id -u) -eq 0 ) { return $true } return $false } }
-#src: https://devblogs.microsoft.com/scripting/use-a-powershell-function-to-see-if-a-command-exists/
-function Test-CommandExists {
-    Param ($command)
-    $oldErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = 'stop'
-    try { Get-Command $command; return $true }
-    catch {return $false}
-    finally { $ErrorActionPreference=$oldErrorActionPreference }
-}    
-
-function Clean-Object {
-    process {
-        $_.PSObject.Properties.Remove('PSComputerName')
-        $_.PSObject.Properties.Remove('RunspaceId')
-        $_.PSObject.Properties.Remove('PSShowComputerName')
-    }
-    #Where-Object { $_.PSObject.Properties.Value -ne $null}
-}
-
-function Get-Environment {  # Get-Variable to show all Powershell Variables accessible via $
-    if ( $args.Count -eq 0 ) {
-        Get-Childitem env:
-    } elseif( $args.Count -eq 1 ) {
-        Start-Process (Get-Command $args[0]).Source
-    } else {
-        Start-Process (Get-Command $args[0]).Source -ArgumentList $args[1..($args.Count-1)]
-    }
-}
-
-function cf {
-    if ( $null -ne $(Get-Module PSFzf) ) {
-        Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
-    } else {
-        Write-Error "please install PSFzf"
-    }
-}
-
-#-------------------------------   Set alias BEGIN    -------------------------------
-$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
-
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$aliasPath = ($profile | Split-Path -Parent)+'\profileAliases.ps1'
-Add-Content -Path $Profile -Value (Get-Content $aliasPath)
-$sw.Stop()
-"loaded aliases"
-$sw.Elapsed.TotalMilliseconds
-
-
-#-------------------------------    Set alias END     -------------------------------
-
-Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
-Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
-Write-Host "Profile:   $PSCommandPath"
-
 <#
  * FileName: Microsoft.PowerShell_profile.ps1
  * Author: perXautomatik
@@ -73,35 +5,34 @@ Write-Host "Profile:   $PSCommandPath"
  * Date: 08/03/2022
  * Copyright: No copyright. You can use this code for anything with no warranty.
 #>
+if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
 
-#loadMessage
-echo "Microsoft.PowerShell_profile.ps1"
+
+Function IIf($If, $IfTrue, $IfFalse) {
+    If ($If) {If ($IfTrue -is "ScriptBlock") {&$IfTrue} Else {$IfTrue}}
+    Else {If ($IfFalse -is "ScriptBlock") {&$IfFalse} Else {$IfFalse}}
+}
+
+
+function timer($script, $interval = 1,$message){
+    $t = [system.diagnostics.stopwatch]::startnew()
+    $job = start-job -script $script
+    while($job.state -eq "running"){
+    $results =  [PSCustomObject]@{
+            messageT = "$message Elapsed: $($t.elapsed) "
+            results = $job | Receive-Job
+        }
+        start-sleep $interval
+    }
+    $t.stop()
+    return $results
+}
 
 # Increase history
 $MaximumHistoryCount = 10000
 
-
-#src: https://stackoverflow.com/a/34098997/7595318
-function Test-IsInteractive {
-    # Test each Arg for match of abbreviated '-NonInteractive' command.
-    $NonInteractiveFlag = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonInteractive' }
-    if ( (-not [Environment]::UserInteractive) -or ( $NonInteractiveFlag -ne $null ) ) {
-        return $false
-    }
-    return $true
-}
-
-#if ( Test-IsInteractive )  { 	(preferably use -noLogo) } # Clear-Host # remove advertisements 
-
-
-function Download-Latest-Profile {
-    New-Item $( Split-Path $($PROFILE.CurrentUserCurrentHost) ) -ItemType Directory -ea 0
-    if ( $(Get-Content "$($PROFILE.CurrentUserCurrentHost)" | Select-String "62a71500a0f044477698da71634ab87b" | Out-String) -eq "" ) {
-        Move-Item -Path "$($PROFILE.CurrentUserCurrentHost)" -Destination "$($PROFILE.CurrentUserCurrentHost).bak"
-    }
-    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/apfelchips/62a71500a0f044477698da71634ab87b/raw/Profile.ps1" -OutFile "$($PROFILE.CurrentUserCurrentHost)"
-    Reload-Profile
-}
 
 # Produce UTF-8 by default
 
@@ -111,18 +42,37 @@ if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
 	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
 }	
 
+$profileFolder = (split-path $profile -Parent)
+
+#-------------------------------   Set Variables BEGIN    -------------------------------
+
+$aliasPath = ($profile | Split-Path -Parent)+'\setVariables.ps1'
+timer -message "Setting variables" -script { Add-Content -Path $Profile -Value (Get-Content $aliasPath) }
+
+#-------------------------------    Set Variables END     -------------------------------
+
+#------------------------------- Import HelperFunctions BEGIN -------------------------------
+$pos = join-path -Path $profileFolder -ChildPath 'functions.ps1'
+timer -message "import HelperFunctions" -script {Import-Module $pos}
+#------------------------------- Import HelperFunctions END   -------------------------------
+
+#-------------------------------   Set alias BEGIN    -------------------------------
+$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
+$aliasPath = ($profile | Split-Path -Parent)+'\profileAliases.ps1'
+timer -message "loading aliases" -script { Add-Content -Path $Profile -Value (Get-Content $aliasPath) }
+
+#-------------------------------    Set alias END     -------------------------------
+
 #------------------------------- Set Paths           -------------------------------
 
 $paths = join-path -Path (split-path $profile -Parent)  -ChildPath 'setPaths.ps1'
-
-Import-Module  $paths
+timer -message "set paths" -script {Import-Module  $paths}
 #------------------------------- Set Paths  end       -------------------------------
 
 #------------------------------- Import Modules BEGIN -------------------------------
 
-$profileFolder = (split-path $profile -Parent)
 $pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
- Import-Module $pos
+timer -message "import modules" -script {Import-Module $pos}
 #------------------------------- Import Modules END   -------------------------------
 
 
@@ -166,21 +116,4 @@ $pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
 
 #------------------------------- SystemMigration end  -------------------------------
 
-#------------------------------- Styling begin --------------------------------------					      
-#change selection to neongreen
-#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
-$colors = @{
-   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
-}
-Set-PSReadLineOption -Colors $colors
 
-# Style default PowerShell Console
-$shell = $Host.UI.RawUI
-
-$shell.WindowTitle= "PS"
-
-$shell.BackgroundColor = "Black"
-$shell.ForegroundColor = "White"
-
-# Load custom theme for Windows Terminal
-#Set-Theme LazyAdmin
