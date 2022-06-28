bbca83b878514c20efc30b80e242fa7c524aa2eb
perXautomatik <christoffer.broback@gmail.com>
2022-06-28 13:53:35 +0200

    [feature] Alias moved to separate file [chore] emptied profile as windows autopopulate this with add content

--- profile.ps1
+++ profile.ps1
@@ -52,78 +52,135 @@ function cf {
 #-------------------------------   Set alias BEGIN    -------------------------------
 $TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
 
-set-alias accelerators       [accelerators]::Get
-set-alias edprofile          start-Notepad-Profile
-set-alias exp-pro            open-ProfileFolder
-set-alias kidStream          get-childitem                          |       out-string -stream
-set-alias history            (Get-PSReadlineOption).HistorySavePath 
-set-alias parameters         get-parameters
-set-alias start-su           start-powershellAsAdmin
-set-alias version            $PSVersionTable                        #       bash-like
-Set-Alias cat                Get-Content                            -Option AllScope
-Set-Alias cd                 Set-Location                           -Option AllScope
-Set-Alias clear              Clear-Host                             -Option AllScope
-Set-Alias cp                 Copy-Item                              -Option AllScope
-Set-Alias history            Get-History                            -Option AllScope
-Set-Alias kill               Stop-Process                           -Option AllScope
-Set-Alias lp                 Out-Printer                            -Option AllScope
-Set-Alias mv                 Move-Item                              -Option AllScope
-Set-Alias ps                 Get-Process                            -Option AllScope
-Set-Alias pwd                Get-Location                           -Option AllScope
-Set-Alias which              Get-Command                            -Option AllScope   
-Set-Alias open               Invoke-Item                            -Option AllScope
-Set-Alias basename           Split-Path                             -Option AllScope
-Set-Alias realpath           Resolve-Path                           -Option AllScope   #       cmd-like Set-Alias rm      Remove-Item -Option   AllScope
-Set-Alias rmdir              Remove-Item                            -Option AllScope
-Set-Alias echo               Write-Output                           -Option AllScope
-Set-Alias cls                Clear-Host                             -Option AllScope   
-Set-Alias chdir              Set-Location                           -Option AllScope
-Set-Alias copy               Copy-Item                              -Option AllScope
-Set-Alias del                Remove-Item                            -Option AllScope
-Set-Alias dir                Get-Childitem                          -Option AllScope
-Set-Alias erase              Remove-Item                            -Option AllScope
-Set-Alias move               Move-Item                              -Option AllScope
-Set-Alias rd                 Remove-Item                            -Option AllScope
-Set-Alias ren                Rename-Item                            -Option AllScope
-Set-Alias set                Set-Variable                           -Option AllScope
-Set-Alias type               Get-Content                            -Option AllScope
-Set-Alias env                Get-Environment                        -Option AllScope   #       custom   aliases
-Set-Alias flush-dns          Clear-DnsClientCache                   -Option AllScope
-Set-Alias touch              Set-FileTime                           -Option AllScope   
-set-alias lsx                get-Childnames                         -Option AllScope
-set-alias filesinfolasstream read-childrenAsStream                  -Option AllScope
-set-alias bcompare           start-bc                               -Option AllScope   
-set-alias GitAdEPathAsSNB    invoke-GitSubmoduleAdd                 -Option AllScope
-set-alias GitUp              invoke-GitLazy                         -Option AllScope
-set-alias gitSilently        invoke-GitLazySilently                 -Option AllScope
-set-alias gitSingleRemote    invoke-gitFetchOrig                    -Option AllScope
-set-alias executeThis        invoke-FuzzyWithEverything             -Option AllScope   
-set-alias filesinfolasstream read-pathsAsStream                     -Option AllScope
-set-alias everything         invoke-Everything                      -Option AllScope
-set-alias make               invoke-Nmake                           -Option AllScope
-set-alias MyAliases          read-aliases                           -Option AllScope
-set-alias OpenAsADmin        invoke-powershellAsAdmin               -Option AllScope
-set-alias home               open-here                              -Option AllScope
-set-alias pastDo             find-historyInvoke                     -Option AllScope
-set-alias pastDoEdit         find-historyAppendClipboard            -Option AllScope
-set-alias HistoryPath        (Get-PSReadlineOption).HistorySavePath -Option AllScope
-set-alias reboot             exit-Nrenter                           -Option AllScope
-set-alias browserflags       start-BrowserFlags                     -Option AllScope
-set-alias df                 get-volume                             -Option AllScope
-set-alias printpaths         read-EnvPaths                          -Option AllScope
-set-alias reload             initialize-profile                     -Option AllScope
-set-alias uptime             read-uptime                            -Option AllScope   
-set-alias getnic             get-mac                                -Option AllScope   #       1.       获取所有      Network Interface   set-alias ll       Get-ChildItem -Option AllScope
-set-alias getip              Get-IPv4Routes                         -Option AllScope
-set-alias getip6             Get-IPv6Routes                         -Option AllScope
-set-alias os-update          Update-Packages                        -Option AllScope
-set-alias remote             invoke-gitRemote                       -Option AllScope   
-set-alias gitsplit           subtree-split-rm-commit                -Option AllScope
-set-alias isFolder           get-isFolder                           -Option AllScope
-set-alias start-powershellAsAdmin invoke-powershellAsAdmin          -Option AllScope
-set-alias psVersion          $PSVersionTable.PSVersion.Major        -Option AllScope 
+$sw = [Diagnostics.Stopwatch]::StartNew()
+$aliasPath = ($profile | Split-Path -Parent)+'\profileAliases.ps1'
+Add-Content -Path $Profile -Value (Get-Content $aliasPath)
+$sw.Stop()
+"loaded aliases"
+$sw.Elapsed.TotalMilliseconds
+
+
 #-------------------------------    Set alias END     -------------------------------
 
 Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
 Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
 Write-Host "Profile:   $PSCommandPath"
+
+<#
+ * FileName: Microsoft.PowerShell_profile.ps1
+ * Author: perXautomatik
+ * Email: christoffer.broback@gmail.com
+ * Date: 08/03/2022
+ * Copyright: No copyright. You can use this code for anything with no warranty.
+#>
+
+#loadMessage
+echo "Microsoft.PowerShell_profile.ps1"
+
+# Increase history
+$MaximumHistoryCount = 10000
+
+
+#src: https://stackoverflow.com/a/34098997/7595318
+function Test-IsInteractive {
+    # Test each Arg for match of abbreviated '-NonInteractive' command.
+    $NonInteractiveFlag = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonInteractive' }
+    if ( (-not [Environment]::UserInteractive) -or ( $NonInteractiveFlag -ne $null ) ) {
+        return $false
+    }
+    return $true
+}
+
+#if ( Test-IsInteractive )  { 	(preferably use -noLogo) } # Clear-Host # remove advertisements 
+
+
+function Download-Latest-Profile {
+    New-Item $( Split-Path $($PROFILE.CurrentUserCurrentHost) ) -ItemType Directory -ea 0
+    if ( $(Get-Content "$($PROFILE.CurrentUserCurrentHost)" | Select-String "62a71500a0f044477698da71634ab87b" | Out-String) -eq "" ) {
+        Move-Item -Path "$($PROFILE.CurrentUserCurrentHost)" -Destination "$($PROFILE.CurrentUserCurrentHost).bak"
+    }
+    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/apfelchips/62a71500a0f044477698da71634ab87b/raw/Profile.ps1" -OutFile "$($PROFILE.CurrentUserCurrentHost)"
+    Reload-Profile
+}
+
+# Produce UTF-8 by default
+
+if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
+	# https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget
+	
+	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
+}	
+
+#------------------------------- Set Paths           -------------------------------
+
+$paths = join-path -Path (split-path $profile -Parent)  -ChildPath 'setPaths.ps1'
+
+Import-Module  $paths
+#------------------------------- Set Paths  end       -------------------------------
+
+#------------------------------- Import Modules BEGIN -------------------------------
+
+$profileFolder = (split-path $profile -Parent)
+$pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
+ Import-Module $pos
+#------------------------------- Import Modules END   -------------------------------
+
+
+#------------------------------- overloading begin
+
+#https://www.sapien.com/blog/2014/10/21/a-better-tostring-method-for-hash-tables/
+
+#better hashtable ToString method
+  Update-TypeData -TypeName "System.Collections.HashTable"   `
+  -MemberType ScriptMethod `
+  -MemberName "ToString" -Value { $hashstr = "@{"; $keys = $this.keys; foreach ($key in $keys) { $v = $this[$key];
+	     if ($key -match "\s") { $hashstr += "`"$key`"" + "=" + "`"$v`"" + ";" }
+	     else { $hashstr += $key + "=" + "`"$v`"" + ";" } }; $hashstr += "}";
+	     return $hashstr }
+#-------------------------------  overloading end
+
+#------------------------------- SystemMigration      -------------------------------
+
+#choco check if installed
+#path to list of aps to install
+#choco ask to install if not present
+
+#list of portable apps,download source
+#path
+#download and extract if not present, ask to confirm
+
+#path to portable apps
+#path to standard download location
+
+
+#git Repos paths and origions,
+#git systemwide profile folder
+#git global path
+
+#everything data folder
+#autohotkey script to run on startup
+
+#startup programs
+
+#reg to add if not present
+
+#------------------------------- SystemMigration end  -------------------------------
+
+#------------------------------- Styling begin --------------------------------------					      
+#change selection to neongreen
+#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
+$colors = @{
+   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
+}
+Set-PSReadLineOption -Colors $colors
+
+# Style default PowerShell Console
+$shell = $Host.UI.RawUI
+
+$shell.WindowTitle= "PS"
+
+$shell.BackgroundColor = "Black"
+$shell.ForegroundColor = "White"
+
+# Load custom theme for Windows Terminal
+#Set-Theme LazyAdmin
