278a6d73ae7de7d49f1b1f52a5bbc711e9744cb2
perXautomatik <christoffer.broback@gmail.com>
2022-06-28 20:08:57 +0200

    [stable] don't clear window

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -254,3 +254,32 @@ set-alias -Name:"version" -Value:"System.Management.Automation.PSVersionHashTabl
 set-alias -Name:"which" -Value:"Get-Command" -Description:"" -Option:"AllScope"
 set-alias -Name:"wjb" -Value:"Wait-Job" -Description:"" -Option:"None"
 
+#------------------------------- Styling begin --------------------------------------					      
+
+if ( (($error.length | group).name -eq $null ) -and  (Test-IsInteractive)   ) { 
+    Clear-Host # remove advertisements (preferably use -noLogo)
+}
+
+#change selection to neongreen
+#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
+$colors = @{
+   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
+}
+Set-PSReadLineOption -Colors $colors
+
+# Style default PowerShell Console
+$shell = $Host.UI.RawUI
+
+$shell.WindowTitle= "PS"
+
+$shell.BackgroundColor = "Black"
+$shell.ForegroundColor = "White"
+
+# Load custom theme for Windows Terminal
+#Set-Theme LazyAdmin
+
+
+Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
+Write-Host "Profile:   $PSCommandPath"
+Write-Host "admin: $isAdmin"
