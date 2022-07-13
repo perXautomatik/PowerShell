1d6b9d5df188b79d7fb61abb7b63e10debcca0a7
perXautomatik <christoffer.broback@gmail.com>
2022-07-13 10:39:19 +0200

    renames before merge

--- prompt.ps1
+++ prompt.ps1
@@ -1,89 +1,145 @@
-#------------------------------- Styling begin --------------------------------------	
+<#
+ * FileName: Microsoft.PowerShell_profile.ps1
+ * Author: perXautomatik
+ * Email: christoffer.broback@gmail.com
+ * Date: 08/03/2022
+ * Copyright: No copyright. You can use this code for anything with no warranty.
+#>
 
-if ( (($error.length | group).name -eq $null ) -and  (Test-IsInteractive)   ) { 
-    Clear-Host # remove advertisements (preferably use -noLogo)
+#loadMessage
+echo "Microsoft.PowerShell_profile.ps1"
+
+# Increase history
+$MaximumHistoryCount = 10000
+
+#------------------------------- Import Modules BEGIN -------------------------------
+
+$profileFolder = (split-path $profile -Parent)
+$pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
+ Import-Module $pos
+#------------------------------- Import Modules END   -------------------------------
+
+
+# Produce UTF-8 by default
+$PSDefaultParameterValues["Out-File:Encoding"]="utf8"
+
+# Show selection menu for tab
+Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+
+#ps ExecutionPolicy;
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+#https://stackoverflow.com/questions/47356782/powershell-capture-git-output
+#Then stderr should be redirected to stdout.
+#set GIT_REDIRECT_STDERR=2>&1
+
+#------------------------------- Set Paths           -------------------------------
+
+$paths = join-path -Path (split-path $profile -Parent)  -ChildPath 'setPaths.ps1'
+
+Import-Module  $paths
+
+
+#------------------------------- Set Paths  end       -------------------------------
+
+#------------------------------- overloading begin
+
+#https://www.sapien.com/blog/2014/10/21/a-better-tostring-method-for-hash-tables/
+
+#better hashtable ToString method
+  Update-TypeData -TypeName "System.Collections.HashTable"   `
+  -MemberType ScriptMethod `
+  -MemberName "ToString" -Value { $hashstr = "@{"; $keys = $this.keys; foreach ($key in $keys) { $v = $this[$key];
+	     if ($key -match "\s") { $hashstr += "`"$key`"" + "=" + "`"$v`"" + ";" }
+	     else { $hashstr += $key + "=" + "`"$v`"" + ";" } }; $hashstr += "}";
+	     return $hashstr }
+#-------------------------------  overloading end
+
+
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
+# 设置预测文本来源为历史记录
+#Set-PSReadLineOption -PredictionSource History
+
+# 每次回溯输入历史，光标定位于输入内容末尾
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# 设置 Tab 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置 Ctrl+d 为退出 PowerShell
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+
+# 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+
+# 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
+# Helper Functions
+#######################################################
+
+function uptimef {
+	Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';
+	EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
 }
-	# Keep the existing window title
 
-if ( $(Test-CommandExists 'get-title') )
-{
+function reloadProfile {
+	& $profile
+}
 
-	$windowTitle = (get-title).Trim()
-
-	if ($windowTitle.StartsWith("Administrator:")) {
-	    $windowTitle = $windowTitle.Substring(14).Trim()
+function find-file($name) {
+	ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
+		$place_path = $_.directory
+		echo "${place_path}\${_}"
 	}
 }
-    $nextId = (get-history -count 1).Id + 1;
-    # KevMar logging
-    $LastCmd = Get-History -Count 1
-    if ($LastCmd) {
-        $lastId = $LastCmd.Id
-        Add-Content -Value "# $($LastCmd.StartExecutionTime)" -Path $PSLogPath
-        Add-Content -Value "$($LastCmd.CommandLine)" -Path $PSLogPath
-        Add-Content -Value '' -Path $PSLogPath
-        $howlongwasthat = $LastCmd.EndExecutionTime.Subtract($LastCmd.StartExecutionTime).TotalSeconds
-    }
-	$currentPath = (get-location).Path.replace($home, "~")
-	$idx = $currentPath.IndexOf("::")
-	if ($idx -gt -1) { $currentPath = $currentPath.Substring($idx + 2) }
 
-	$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-	$windowsPrincipal = new-object 'System.Security.Principal.WindowsPrincipal' $windowsIdentity
-
-    # Kerazy_POSH propmt
-    # Get Powershell version information
-    $MajorVersion = $PSVersionTable.PSVersion.Major
-    $MinorVersion = $PSVersionTable.PSVersion.Minor
-
-    # Detect if the Shell is 32- or 64-bit host
-    if ([System.IntPtr]::Size -eq 8) {
-        $ShellBits = 'x64 (64-bit)'
-    } elseif ([System.IntPtr]::Size -eq 4) {
-        $ShellBits = 'x86 (32-bit)'
-    }
-
-    # Set Window Title to display Powershell version info, Shell bits, username and computername
-    $host.UI.RawUI.WindowTitle = "PowerShell v$MajorVersion.$MinorVersion $ShellBits | $env:USERNAME@$env:USERDNSDOMAIN | $env:COMPUTERNAME | $env:LOGONSERVER"
-
-    # Set Prompt Line 1 - include Date, file path location
-    Write-Host(Get-Date -UFormat "%Y/%m/%d %H:%M:%S ($howlongwasthat) | ") -NoNewline -ForegroundColor DarkGreen
-    Write-Host(Get-Location) -ForegroundColor DarkGreen
-
-    # Set Prompt Line 2
-    # Check for Administrator elevation
-    if (Test-IsAdmin) {
-        Write-Host '# ADMIN # ' -NoNewline -ForegroundColor Cyan
-    } else {        
-        Write-Host '# User # ' -NoNewline -ForegroundColor DarkCyan
-    }
-
-    if ($psISE) { $color = "Black"; }
-    elseif ($windowsPrincipal.IsInRole("Administrators") -eq 1)
-    { $color = "Yellow";}
-    else{ $color = "Green";}
-
-if ( $(Test-CommandExists 'Write-HgStatus') )
-{
-	Write-HgStatus (Get-HgStatus)
-	Write-GitStatus (Get-GitStatus)
-}
-	write-host (" [" + $nextId + "]") -NoNewLine -ForegroundColor $color
-	if ((get-location -stack).Count -gt 0) { write-host ("+" * ((get-location -stack).Count)) -NoNewLine -ForegroundColor Cyan }
-
-
-if ( $(Test-CommandExists 'set-title') )
-{    $title = $currentPath  
-    if ($windowTitle -ne $null) { $title = ($title + "  »  " + $windowTitle) }
-	set-title $title
+function printpath {
+	($Env:Path).Split(";")
 }
 
-	return " "
 
-    Write-Host '»' -NoNewLine -ForeGroundColor Green
-    ' ' # need this space to avoid the default white PS>  
+function unzipf ($file) {
+	$dirname = (Get-Item $file).Basename
+	echo("Extracting", $file, "to", $dirname)
+	New-Item -Force -ItemType directory -Path $dirname
+	expand-archive $file -OutputPath $dirname -ShowProgress
+}
+#------------------------------- SystemMigration      -------------------------------
+
+#choco check if installed
+#path to list of aps to install
+#choco ask to install if not present
+
+#list of portable apps,download source
+#path
+#download and extract if not present, ask to confirm
+
+#path to portable apps
+#path to standard download location
 
 
+#git Repos paths and origions,
+#git systemwide profile folder
+#git global path
+
+#everything data folder
+#autohotkey script to run on startup
+
+#startup programs
+
+#reg to add if not present
+
+#------------------------------- SystemMigration end  -------------------------------
+
+#------------------------------- Styling begin --------------------------------------					      
+#change selection to neongreen
+#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
+$colors = @{
+   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
+}
+Set-PSReadLineOption -Colors $colors
 
 # Style default PowerShell Console
 $shell = $Host.UI.RawUI
@@ -93,17 +149,5 @@ $shell.WindowTitle= "PS"
 $shell.BackgroundColor = "Black"
 $shell.ForegroundColor = "White"
 
-$colors = $host.PrivateData
-$colors.verbosebackgroundcolor = "Magenta"
-$colors.verboseforegroundcolor = "Green"
-$colors.warningbackgroundcolor = "Red"
-$colors.warningforegroundcolor = "white"
-$colors.ErrorBackgroundColor = "DarkCyan"
-$colors.ErrorForegroundColor = "Yellow"
-
 # Load custom theme for Windows Terminal
 #Set-Theme LazyAdmin
-Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
-Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
-Write-Host "Profile:   $PSCommandPath"
-Write-Host "admin: $isAdmin"
