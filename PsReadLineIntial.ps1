2527ee862b0d0003706f701ceb6040b22d04265f
perXautomatik <christoffer.broback@gmail.com>
2022-07-26 18:19:58 +0200

    [Chore]

--- PsReadLineIntial.ps1
+++ PsReadLineIntial.ps1
@@ -1,82 +1,164 @@
 <#
-    # 设置 PowerShell 主题
+ * FileName: Microsoft.PowerShell_profile.ps1
  * Author: 刘 鹏
  * Email: littleNewton6@outlook.com
  * Date: 2021, Aug. 21
-   # 引入 ps-read-line # useful history related actions      
-   # example: https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1
+ * Copyright: No copyright. You can use this code for anything with no warranty.
 #>
-   if (Test-ModuleExists 'PSReadLine')
-    {
+
+
+#------------------------------- Import Modules BEGIN -------------------------------
+# 引入 posh-git
+Import-Module posh-git
+
+# 引入 oh-my-posh
+Import-Module oh-my-posh
+
 # 引入 ps-read-line
- 	    if(!(TryImport-Module PSReadLine)) #null if fail to load
-        {        
-         
-            #-------------------------------  Set Hot-keys BEGIN  -------------------------------
-        
-            $PSReadLineOptions = @{
-                HistorySavePath = $global:historyPath
+Import-Module PSReadLine
+
+# 设置 PowerShell 主题
+# Set-PoshPrompt ys
+Set-PoshPrompt paradox
+#------------------------------- Import Modules END   -------------------------------
+
+
+
+
+
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
 # 设置预测文本来源为历史记录
-                PredictionSource = "HistoryAndPlugin"
+Set-PSReadLineOption -PredictionSource History
+
 # 每次回溯输入历史，光标定位于输入内容末尾
-                HistorySearchCursorMovesToEnd = $true                        
-            }
-            
-            Set-PSReadLineOption @PSReadLineOptions
-            
-            echo ($host.Name -eq 'ConsoleHost')
-            # Set-PSReadLineOption -EditMode Emac
-            
-            # 每次回溯输入历史，光标定位于输入内容末尾    
-            # 设置 Tab 为菜单补全和 Intellisense    
-            # 设置 Ctrl+d 为退出 PowerShell
-            # 设置 Ctrl+z 为撤销
-            # 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
-                Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete       
-            # 设置 Ctrl+d 为退出 PowerShell
-            Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
-                Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
-            # 设置 Ctrl+z 为撤销
-                Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
-            # 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
-            Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# 设置 Tab 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置 Ctrl+d 为退出 PowerShell
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+
+# 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+
+# 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
 # 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
 
 
-            if ( $null -ne $(Get-Module PSFzf)  ) {
-                #Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-                #$FZF_COMPLETION_TRIGGER='...'
-                Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-            }
-                Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-                #-------------------------------  Set Hot-keys END    -------------------------------
-
-                if ( $(Get-Module PSReadline).Version -ge 2.2 ) {
-                    # 设置预测文本来源为历史记录
-                    Set-PSReadLineOption -predictionsource history -ea SilentlyContinue
-                }
 
 
-                if ( $(Test-CommandExists 'Set-PSReadLineOption') )
-                {
-                    #------------------------------- Styling begin --------------------------------------					      
-                    #change selection to neongreen
-                    #https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
-                    $colors = @{
-                    "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
-                    }
-                    Set-PSReadLineOption -Colors $colors
-                }
-                echo "historyPath: $global:historyPath"
-        }
-        else
-        {
-            Write-Verbose "psReadLineNotimported"
-        }
-    }
-    else
-    {
-        Write-Verbose "psReadLineNotpresent"
-    }
+
+#-------------------------------    Functions BEGIN   -------------------------------
+# Python 直接执行
+$env:PATHEXT += ";.py"
+
+# 更新系统组件
+function Update-Packages {
+	# update pip
+	# Write-Host "Step 1: 更新 pip" -ForegroundColor Magenta -BackgroundColor Cyan
+	# $a = pip list --outdated
+	# $num_package = $a.Length - 2
+	# for ($i = 0; $i -lt $num_package; $i++) {
+	# 	$tmp = ($a[2 + $i].Split(" "))[0]
+	# 	pip install -U $tmp
+	# }
+
+	# update conda
+	Write-Host "Step 1: 更新 Anaconda base 虚拟环境" -ForegroundColor Magenta -BackgroundColor Cyan
+	conda activate base
+	conda upgrade python
+
+	# update TeX Live
+	$CurrentYear = Get-Date -Format yyyy
+	Write-Host "Step 2: 更新 TeX Live" $CurrentYear -ForegroundColor Magenta -BackgroundColor Cyan
+	tlmgr update --self
+	tlmgr update --all
+
+	# update Chocolotey
+	# Write-Host "Step 3: 更新 Chocolatey" -ForegroundColor Magenta -BackgroundColor Cyan
+	# choco outdated
+
+	# update Apps using winget
+	Write-Host "Step 3: 通过 winget 更新 Windows 应用程序" -ForegroundColor Magenta -BackgroundColor Cyan
+	winget upgrade
+	winget upgrade --all
+	
+}
+#-------------------------------    Functions END     -------------------------------
+
+
+
+
+
+#-------------------------------   Set Alias BEGIN    -------------------------------
+# 1. 编译函数 make
+function MakeThings {
+	nmake.exe $args -nologo
+}
+Set-Alias -Name make -Value MakeThings
+
+# 2. 更新系统 os-update
+Set-Alias -Name os-update -Value Update-Packages
+
+# 3. 查看目录 ls & ll
+function ListDirectory {
+	(Get-ChildItem).Name
+	Write-Host("")
+}
+Set-Alias -Name ls -Value ListDirectory
+Set-Alias -Name ll -Value Get-ChildItem
+
+# 4. 打开当前工作目录
+function OpenCurrentFolder {
+	param
+	(
+		# 输入要打开的路径
+		# 用法示例：open C:\
+		# 默认路径：当前工作文件夹
+		$Path = '.'
+	)
+	Invoke-Item $Path
+}
+Set-Alias -Name open -Value OpenCurrentFolder
+
+# 5. 更改工作目录
+function Change-Directory {
+	param (
+		# 输入要切换到的路径
+		# 用法示例：cd C:/
+		# 默认路径：D 盘的桌面
+		$Path = 'D:\Users\newton\Desktop\'
+	)
+	Set-Location $Path
+}
+Set-Alias -Name cd -Value Change-Directory -Option AllScope
+#-------------------------------    Set Alias END     -------------------------------
+
+
+
+
+
+#-------------------------------   Set Network BEGIN    -------------------------------
+# 1. 获取所有 Network Interface
+function Get-AllNic {
+	Get-NetAdapter | Sort-Object -Property MacAddress
+}
+Set-Alias -Name getnic -Value Get-AllNic
+
+# 2. 获取 IPv4 关键路由
+function Get-IPv4Routes {
+	Get-NetRoute -AddressFamily IPv4 | Where-Object -FilterScript {$_.NextHop -ne '0.0.0.0'}
+}
+Set-Alias -Name getip -Value Get-IPv4Routes
+
+# 3. 获取 IPv6 关键路由
+function Get-IPv6Routes {
+	Get-NetRoute -AddressFamily IPv6 | Where-Object -FilterScript {$_.NextHop -ne '::'}
+}
+Set-Alias -Name getip6 -Value Get-IPv6Routes
+#-------------------------------    Set Network END     -------------------------------
