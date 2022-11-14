dad02e290604c2d629f647a835d92e8651ec95f1
perXautomatik <christoffer.broback@gmail.com>
2022-11-14 15:25:00 +0100

    [functions/setPaths] RetriveFromCache Standardized loading of paths [setpaths] Renamed

--- _Configurators/setPaths.ps1
+++ _Configurators/setPaths.ps1
@@ -1,4 +1,3 @@
-
 #ps setHistorySavePath
 if (-not $env:XDG_CONFIG_HOME) { $env:XDG_CONFIG_HOME = Join-Path -Path "$HOME" -ChildPath ".config" }; $XDG_CONFIG_HOME = $env:XDG_CONFIG_HOME
 if (-not $env:XDG_DATA_HOME) { $env:XDG_DATA_HOME = Join-Path -Path "$HOME" -ChildPath ".local/share" }; $XDG_DATA_HOME = $env:XDG_DATA_HOME
@@ -14,61 +13,57 @@ if (-not $env:DEVEL_DIR) { $env:DEVEL_DIR = Join-Path -Path "$HOME" -ChildPath "
 if (-not $env:PORTS_DIR) { $env:PORTS_DIR = Join-Path -Path "$HOME" -ChildPath "ports" }; $PORTS_DIR = $env:PORTS_DIR
 
 # Load scripts from the following locations   
+$profileFolder = (split-path $profile -Parent)
 
-$EnvPath = join-path -Path $home -ChildPath 'Documents\WindowsPowerShell\snipps\snipps$'
+
+
+$fileToFind = '\snipps'
+$workpath = join-path -Path $home -ChildPath 'Documents\WindowsPowerShell\$fileToFind' ;
+$EnvPath = (retrive-FromCache -SearchString $fileToFind -externalPath $workpath )
 $env:Path += ";$EnvPath"
 
-$historyPath = "$home\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
-set-PSReadlineOption -HistorySavePath $historyPath 
-echo "historyPath: $historyPath"
+
+$fileToFind = 'ConsoleHost_history.txt'
+$workpath =  "$home\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadline\$fileToFind" ; 
+set-PSReadlineOption -HistorySavePath (retrive-FromCache -SearchString $fileToFind -externalPath $workpath )
 
 #$path = [Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
 
 # vscode Portable Path
-$vscodepath = 'D:\portapps\6, Text,programming, x Editing\PortableApps\vscode-portable\vscode-portable.exe'
-[Environment]::SetEnvironmentVariable("code", $vscodepath)
+
+$fileToFind = 'vscode-portable.exe'
+$workpath =  "D:\portapps\6, Text,programming, x Editing\PortableApps\vscode-portable\$fileTofind"  ; 
+[Environment]::SetEnvironmentVariable("code", (retrive-FromCache -SearchString $fileToFind -externalPath $workpath ))
 
 #sqlite dll
-$workpath = "C:\Program Files\System.Data.SQLite\2010\bin\System.Data.SQLite.dll"  ; 
-cd $home
-$cache = get-content '.\appdata\Roaming\Everything\Run History.csv' | ConvertFrom-Csv | ? { ($_.filename | Test-Path) } | %{[system.io.fileinfo]$_.filename} | ? {$_.name -eq 'System.Data.SQLite.DLL'}
-$q = $cache.fullname
-$p = if(Test-Path $workpath){$workpath} else {
+$fileToFind = 'System.Data.SQLite.dll'
+$workpath = "C:\Program Files\System.Data.SQLite\2010\bin\$fileTofind"  ; 
+Add-Type -Path (retrive-FromCache -SearchString $fileToFind -externalPath $workpath )
 
-if ( $(Test-CommandExists 'everything') ) {$alternative = (everything 'wfn:System.Data.SQLite.DLL')[0] ;}
-$alternative
 
-} ;
-
-$p = $p ?? 'unable to set path' 
-
-		if( Test-Path $p ) 
-		{
-			Add-Type -Path $p
-			echo $p 
-		}
 
 ### local variables
-$cache2 = get-content '.\appdata\Roaming\Everything\Run History.csv' | ConvertFrom-Csv | ? { ($_.filename | Test-Path) } | %{[system.io.fileinfo]$_.filename} | ? {$_.name -eq 'whatpulse.db'}
-$watPulseDbPath = $cache2.fullname
-$whatPulseDbPath = if(Test-Path $watPulseDbPath){$watPulseDbPath} else {
+
 $whatPulseDbQuery = "select rightstr(path,instr(reverse(path),'/')-1) exe,path from (select max(path) path,max(cast(replace(version,'.','') as integer)) version from applications group by case when online_app_id = 0 then name else online_app_id end)"
-if ( $(Test-CommandExists 'everything') ) {$whatPulseDbPath = (Everything 'whatpulse.db')[0]; }
-}
+
+$fileToFind = 'whatpulse.db'
+$whatPulseDbPath = (retrive-FromCache -SearchString $fileToFind)
 
 [Environment]::SetEnvironmentVariable("WHATPULSE_DB", $whatPulseDbPath)
 if (-not $env:WHATPULSE_DB) { $env:WHATPULSE_DB = $whatPulseDbPath }; $WHATPULSE_DB = $env:WHATPULSE_DB
 
-
 [Environment]::SetEnvironmentVariable("WHATPULSE_QUERY", $whatPulseDbQuery)
 if (-not $env:WHATPULSE_QUERY) { $env:WHATPULSE_QUERY = $whatPulseDbQuery }; $WHATPULSE_QUERY = $env:WHATPULSE_QUERY
 
-
-
-
-$datagripPath = '$home\appdata\Roaming\JetBrains\DataGrip2021.1'
+$fileToFind = 'DataGrip2021.1'
+$workpath = "$home\appdata\Roaming\JetBrains\$fileTofind"  ; 
+$datagripPath = (retrive-FromCache -SearchString $fileToFind -externalPath $workpath )
 [Environment]::SetEnvironmentVariable("datagripPath", $datagripPath)
-$bComparePath = 'D:\PortableApps\2. fileOrganization\PortableApps\Beyond Compare 4'
+
+
+$fileToFind = 'Beyond Compare 4'
+$workpath = "D:\PortableApps\2. fileOrganization\PortableApps\$fileTofind"  ; 
+$bComparePath = (retrive-FromCache -SearchString $fileToFind -externalPath $workpath )
 [Environment]::SetEnvironmentVariable("bComparePath", $bComparePath)
 
 #-------------------------------    Functions END     -------------------------------
