a78d635e9790fc0b0fdd273b819260a9137f8989
perXautomatik <christoffer.broback@gmail.com>
2023-05-02 18:28:25 +0200

    state

---
+++ csharp - projects.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1678266845397" ID="ID_1913812075" MODIFIED="1678266857290" TEXT="csharp - projects">
+<node CREATED="1678266857834" ID="ID_1016519805" MODIFIED="1678266870525" POSITION="right" TEXT="folder mapping solution">
+<node CREATED="1678266875753" ID="ID_704746880" MODIFIED="1678266888440" TEXT="just like a orm, but generating renaming pairs"/>
+</node>
+<node CREATED="1678266890274" ID="ID_1081260372" MODIFIED="1678266906817" POSITION="right" TEXT="json visualizer and node organizer"/>
+<node CREATED="1678266908907" ID="ID_1214584950" MODIFIED="1678349842820" POSITION="right" TEXT="should be classes loadable in dlls"/>
+</node>
+</map>
---
+++ fallout new vegas modding.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1682315044963" ID="ID_1964130809" MODIFIED="1682315064827" TEXT="fallout new vegas modding">
+<node CREATED="1682315065218" ID="ID_1903411183" MODIFIED="1682315097083" POSITION="right" TEXT="scriptrunner to distribute creatures by faction rather than by static text">
+<node CREATED="1682315100570" ID="ID_1431830643" MODIFIED="1682315117032" TEXT="this would increase variety"/>
+<node CREATED="1682315117504" ID="ID_1036933008" MODIFIED="1682315137907" TEXT="would need to build lists before distrobuting though"/>
+</node>
+<node CREATED="1682315139896" ID="ID_1922360764" MODIFIED="1682315225205" POSITION="left" TEXT="scriptrunner to purge all lvl tables and to to rebuild according to items actual value and properties">
+<node CREATED="1682315232400" ID="ID_208015570" MODIFIED="1682315251057" TEXT="ressulting in always having shit gear till you lvled and earned otherwise"/>
+</node>
+<node CREATED="1682315253042" ID="ID_667974948" MODIFIED="1682315262616" POSITION="right" TEXT="pipeweapons from fallout 4"/>
+<node CREATED="1682315263594" ID="ID_1482703421" MODIFIED="1682315263594" POSITION="right" TEXT=""/>
+<node CREATED="1682410469193" ID="ID_1206857689" MODIFIED="1682410478253" POSITION="left" TEXT="A better weapon modding interface">
+<node CREATED="1682410479357" ID="ID_1702258795" MODIFIED="1682410489352" TEXT="based on real time modding interface?"/>
+<node CREATED="1682410620515" ID="ID_691992664" MODIFIED="1682410708778" TEXT="see which ingredents missing ">
+<node CREATED="1682410537389" ID="ID_109992643" MODIFIED="1682410561046" TEXT="generate mod categories scriptwise">
+<node CREATED="1682410563841" ID="ID_373133584" MODIFIED="1682410619270" TEXT="based on recepies components interface view as it dynamically builds the list "/>
+<node CREATED="1682410709221" ID="ID_271765405" MODIFIED="1682410722537" TEXT="recursive list built of need material to craft and where "/>
+<node CREATED="1682410723167" ID="ID_1537167309" MODIFIED="1682410739576" TEXT="plan crafting by thinking of how to improve your current weapon"/>
+</node>
+</node>
+<node CREATED="1682410489839" ID="ID_1884038846" MODIFIED="1682410535222" TEXT="from mod interface go to craft interface for weapon specificly"/>
+</node>
+<node CREATED="1682410834068" ID="ID_1180785015" MODIFIED="1682410836486" POSITION="right" TEXT="enemy ai">
+<node CREATED="1682410796561" ID="ID_1111006061" MODIFIED="1682410830376" TEXT="enemies seeking cover better"/>
+</node>
+<node CREATED="1682410842077" ID="ID_1408491546" MODIFIED="1682410871730" POSITION="left" TEXT="distrobute armor attachments, ">
+<node CREATED="1682410872874" ID="ID_136287133" MODIFIED="1682410922208" TEXT="pool items by slot"/>
+<node CREATED="1682410923433" ID="ID_1077590002" MODIFIED="1682410943928" TEXT="distribute to each npc"/>
+</node>
+</node>
+</map>
