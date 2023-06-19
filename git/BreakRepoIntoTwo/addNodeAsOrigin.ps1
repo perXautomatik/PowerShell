43c21b7054a8e815e9a53089d7fefc1b752d1e63
perXautomatik <christoffer.broback@gmail.com>
2023-06-19 13:00:26 +0200

    Described thanks to Bing Chat (chatGpt)

--- git/BreakRepoIntoTwo/addNodeAsOrigin.ps1
+++ git/BreakRepoIntoTwo/addNodeAsOrigin.ps1
@@ -1,29 +1,57 @@
-C:
+<#
+.SYNOPSIS
+Split a folder from a parent repository into a new repository using git subtree.
 
-cd "C:\Users\crbk01\Desktop\lib-repo"
+.DESCRIPTION
+This script takes three parameters: the path to the new repository, the path to the parent repository, and the folder name of the node to split. It then performs the following steps:
 
-$node= Read-Host -Prompt "Node Origion Path/url"
+- Change directory to the new repository and add a remote origin for the node.
+- Change directory to the parent repository and use the subtree split command to put the node folder in a separate branch.
+- Push the contents of the split branch to the new repository using the file path.
+- Remove the node folder from the parent repository and add it back as a subtree from the new repository.
 
-git remote add node $node
+.PARAMETER NewRepoPath
+The path to the new repository where the node folder will be split into.
 
+.PARAMETER ParentRepoPath
+The path to the parent repository where the node folder resides.
 
-#From within your parent project folder, use the subtree split command and put the lib folder in a separate branch:
+.PARAMETER NodeFolderName
+The folder name of the node to split into a new repository.
 
-$parent= Read-Host -Prompt "Path where parrent of node resides"
-$folder= Read-Host -Prompt "Foldername of node to brake into new repo"
+.EXAMPLE
+PS C:\> Split-Folder -NewRepoPath "C:\Users\crbk01\Desktop\lib-repo" -ParentRepoPath "C:\Users\crbk01\Desktop\parent-repo" -NodeFolderName "lib"
 
-cd $parent
+This example splits the "lib" folder from the "C:\Users\crbk01\Desktop\parent-repo" into a new repository at "C:\Users\crbk01\Desktop\lib-repo".
+#>
 
-git subtree split --prefix=$folder -b split
+function Split-Folder {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$NewRepoPath,
+        [Parameter(Mandatory=$true)]
+        [string]$ParentRepoPath,
+        [Parameter(Mandatory=$true)]
+        [string]$NodeFolderName
+    )
 
-#Push the contents to the of the split branch to your newly created bare repo using the file path to the repository.
+    # Change directory to the new repository and add a remote origin for the node
+    cd $NewRepoPath
+    $node = Read-Host -Prompt "Node Origin Path/url"
+    git remote add node $node
 
-git push C:\Users\crbk01\Desktop\lib-repo split:master
+    # Change directory to the parent repository and use the subtree split command to put the node folder in a separate branch
+    cd $ParentRepoPath
+    git subtree split --prefix=$NodeFolderName -b split
 
-#Now that lib folder lives in it’s new repository, you need to remove it from the parent repository and add the subtree back, from it’s new repository:
+    # Push the contents of the split branch to the new repository using the file path
+    git push $NewRepoPath split:master
 
-git remote add $folder <url_to_lib_remote>
-git rm -r $folder
-git add -A
-git commit -am "removing $folder folder"
-git subtree add --prefix=$folder $folder master
+    # Remove the node folder from the parent repository and add it back as a subtree from the new repository
+    git remote add $NodeFolderName <url_to_lib_remote>
+    git rm -r $NodeFolderName
+    git add -A
+    git commit -am "removing $NodeFolderName folder"
+    git subtree add --prefix=$NodeFolderName $NodeFolderName master
+}
