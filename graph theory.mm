fdcff1e5dd75e00cde0b5f9ac3da3a80cadb25bc
perXautomatik <christoffer.broback@gmail.com>
2023-07-18 11:08:29 +0200

    raw state, missing MindMapps

--- graph theory.mm
+++
@@ -1,6 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1678181468353" ID="ID_182954502" MODIFIED="1678181481375" TEXT="graph theory">
-<node CREATED="1678181481758" ID="ID_1823928366" MODIFIED="1678181494868" POSITION="right" TEXT="finding dupe folders"/>
-</node>
-</map>
