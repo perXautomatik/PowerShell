da8614deff47dc6c728ebb1a1a13537f5d8746a0
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-07-19 12:32:56 +0200

    some renaming

--- Snipps/jsonToCsv.ps1
+++ Snipps/jsonToCsv.ps1
@@ -17,15 +17,11 @@ foreach ($p in $hash.GetEnumerator())
 };$ressult
 }}
 
-<<<<<<< HEAD
-. ./unionObject
-=======
 Function Union-Object ([String[]]$Property = @()) {             # https://powersnippets.com/union-object/
     $Objects = $Input | ForEach {$_}                            # Version 02.00.01, by iRon
     If (!$Property) {ForEach ($Object in $Objects) {$Property += $Object.PSObject.Properties | Select -Expand Name}}
     $Objects | Select ([String[]]($Property | Select -Unique))
 } Set-Alias Union Union-Object
->>>>>>> 9f360da (asd)
 
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | out-null
