dcd9b0e10daffa3d1771ec4fee739d610871c246
perXautomatik <christoffer.broback@gmail.com>
2023-07-29 13:45:19 +0200

    etc

--- subtree/addUnknown.ps1
+++ subtree/addUnknown.ps1
@@ -1,15 +1,8 @@
-﻿# Define the path to search for .git files
-$path = "B:\d2rmm\mods\"
-
-# Define the path to the Everything command line tool
-$es = "C:\ProgramData\scoop\shims\es.exe"
-# Get the list of .git files using Everything
-
-# Get the list of local git repositories
-$listx = & $es -p $path -s -regex "[.]git.config$"
+﻿# Get the list of local git repositories
+$listx = Get-Content "listx.txt"
 
 # Get the other local repository directory
-$dirx = $path
+$dirx = "C:\dirx"
 
 # Change the working directory to dirx
 Set-Location $dirx
@@ -17,18 +10,14 @@ Set-Location $dirx
 # Loop through each repository in the list
 foreach ($repo in $listx) {
     # Get the relative path from dirx to repo
-    $repo = $repo | Split-Path -Parent
-    $repo = $repo | Split-Path -Parent
     $relpath = [System.IO.Path]::GetRelativePath($dirx, $repo)
 
-    git add .
     # Use git index to remove memory for the relative path in dirx
-    git rm --cached -r $relpath
+    git rm --cached $relpath
 
     # Commit the changes
     git commit -m "Remove memory for $relpath"
 
     # Add the repo as a submodule to dirx at the relative path
     git submodule add $repo $relpath
-    git commit --amend
 }
