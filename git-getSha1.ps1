7ba766104f76b04aa1627918b49f8f07b1fdb5a2
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-08-21 16:24:42 +0200

    can you rewrite this script Into a number of functions, get-sha1, get-files-sha1, so that calling the cd $localrepopath ; get-sha1 | get-files-sha1 produce the same as the script without functions

--- git-getSha1.ps1
+++ git-getSha1.ps1
@@ -1,56 +1,138 @@
-# A PowerShell script that takes a path to a local git repo as input
-# and then for each commit on the current branch, print file names, file sizes and checksums of files affected by each commit
+# A function that takes a path to a local git repo as input
+# and returns the list of commits on the current branch
+Function Get-Sha1 {
+    Param (
+        # The path to the local git repo
+        [Parameter(Mandatory=$true)]
+        [string]$RepoPath
+    )
 
-# Get the path to the local git repo from the user
-$repoPath = Read-Host -Prompt "Enter the path to the local git repo"
+    # Change the current directory to the repo path
+    Set-Location $RepoPath
 
-# Change the current directory to the repo path
-Set-Location $repoPath
+    # Get the current branch name
+    $BranchName = git rev-parse --abbrev-ref HEAD
 
-# Get the current branch name
-$branchName = git rev-parse --abbrev-ref HEAD
+    # Get the list of commits on the current branch
+    $Commits = git rev-list $BranchName
 
-# Get the list of commits on the current branch
-$commits = git rev-list $branchName
+    # Return the list of commits
+    return $Commits
+}
 
-# Loop through each commit
-foreach ($commit in $commits) {
-    # Print the commit hash and message
-    $commitMessage = git log -1 --format=%s $commit
-    Write-Host "Commit: $commit - $commitMessage"
+# A function that takes a list of commits as input
+# and prints file names, file sizes and checksums of files affected by each commit
+Function Get-FilesSha1 {
+    Param (
+        # The list of commits
+        [Parameter(ValueFromPipeline=$true)]
+        [string[]]$Commits,
 
-    # Get the list of files affected by the commit
-    $files = git diff-tree --no-commit-id --name-only -r $commit
+        # The path to the local git repo
+        [Parameter(Mandatory=$true)]
+        [string]$RepoPath,
 
-    # Loop through each file
-    foreach ($file in $files) {
-        # Get the file size in bytes
-        $fileSize = (Get-Item $file).Length
+        # The flag to suppress errors and continue
+        [switch]$Force
+    )
 
-        # Format the file size to human-readable units
-        $fileSizeFormatted = [math]::Round($fileSize / 1KB, 2)
-        if ($fileSizeFormatted -ge 1MB) {
-            $fileSizeFormatted = [math]::Round($fileSize / 1MB, 2) + " MB"
+    Begin {
+        # Create an empty array to store the output
+        $Output = @()
+
+        # Check if the path is a valid git repository
+        $IsGitRepo = git -C $RepoPath rev-parse 2>/dev/null
+
+        # If not, throw an error or write a warning, depending on the Force flag
+        if (-not $IsGitRepo) {
+            if ($Force) {
+                Write-Warning "The path $RepoPath is not a valid git repository. Skipping..."
+                return
+            }
+            else {
+                Throw "The path $RepoPath is not a valid git repository. Use -Force to ignore this error and continue."
+            }
         }
-        elseif ($fileSizeFormatted -ge 1KB) {
-            $fileSizeFormatted = [math]::Round($fileSize / 1KB, 2) + " KB"
-        }
-        else {
-            $fileSizeFormatted = $fileSize + " B"
-        }
 
-        # Get the file content as it is stored in the commit
-        $fileContent = git show $commit:$file
+        # Change the current directory to the repo path
+        Set-Location $RepoPath
 
-        # Calculate the checksum for the file content using MD5 algorithm
-        $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-        $utf8 = New-Object -TypeName System.Text.UTF8Encoding
-        $checksum = [System.BitConverter]::ToString ($md5.ComputeHash ($utf8.GetBytes ($fileContent)))
-
-        # Print the file name, size and checksum
-        Write-Host "$file - $fileSizeFormatted - $checksum"
+        # Get the current branch name
+        $BranchName = git rev-parse --abbrev-ref HEAD
     }
 
-    # Print a blank line for readability
-    Write-Host ""
+    Process {
+        
+        # Loop through each commit in the input
+        foreach ($Commit in $Commits) {
+            # Check if the commit exists in the current branch
+            $IsCommitValid = git merge-base --is-ancestor $Commit $BranchName 2>/dev/null
+
+            # If not, throw an error or write a warning, depending on the Force flag
+            if (-not $IsCommitValid) {
+                if ($Force) {
+                    Write-Warning "The commit $Commit does not exist in the current branch $BranchName. Skipping..."
+                    continue
+                }
+                else {
+                    Throw "The commit $Commit does not exist in the current branch $BranchName. Use -Force to ignore this error and continue."
+                }
+            }
+
+            # Get the commit hash and message
+            $CommitMessage = git log -1 --format=%s $Commit
+
+            # Get the list of files affected by the commit
+            $Files = git diff-tree --no-commit-id --name-only -r $Commit
+
+            # Loop through each file in the commit
+            foreach ($File in $Files) {
+                # Get the file size in bytes
+                $FileSize = (Get-Item $File).Length
+
+                # Format the file size to human-readable units
+                $FileSizeFormatted = [math]::Round($FileSize / 1KB, 2)
+                if ($FileSizeFormatted -ge 1MB) {
+                    $FileSizeFormatted = [math]::Round($FileSize / 1MB, 2) + " MB"
+                }
+                elseif ($FileSizeFormatted -ge 1KB) {
+                    $FileSizeFormatted = [math]::Round($FileSize / 1KB, 2) + " KB"
+                }
+                else {
+                    $FileSizeFormatted = $FileSize + " B"
+                }
+
+                # Get the file content as it is stored in the commit
+                $u = "git show $Commit"+":"+"$File"
+                $FileContent = invoke-expression $u
+
+                # Calculate the checksum for the file content using MD5 algorithm
+                $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+                $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+                $b = $utf8.GetBytes($FileContent)
+                $h = $md5.ComputeHash($b)
+                $Checksum = [System.BitConverter]::ToString($h)
+
+                # Create a custom object to store the file information
+                $FileInfo = [PSCustomObject]@{
+                    Commit = $Commit
+                    Message = $CommitMessage
+                    File = $File
+                    Size = $FileSizeFormatted
+                    Checksum = $Checksum
+                }
+
+                # Add the custom object to the output array
+                $Output += $FileInfo
+            }
+        }
+    }
+
+    End {
+        # Return the output array
+        return $Output
+    }
 }
+
+
+cd $localrepopath ; Get-Sha1 | Get-FilesSha1 | Format-Table -AutoSize -Wrap -GroupBy Commit 
