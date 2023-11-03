c9f0e70fa40746511611c6aa6ad6762ef425c1c7
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-11-03 15:41:01 +0100

    Further reordering,
    # Conflicts:
    #	.gitignore
    #	Microsoft.PowerShell_profile.ps1
    #	_Configurators/PsReadLineIntial.psm1
    #	functions.psm1

--- _Configurators/PsReadLineIntial.psm1
+++ _Configurators/PsReadLineIntial.psm1
@@ -13,21 +13,28 @@
         {        
          
 
-#-------------------------------  Set Hot-keys BEGIN  -------------------------------
-        
-            $PSReadLineOptions = @{
-                HistorySavePath = $global:historyPath
-# 设置预测文本来源为历史记录
-                PredictionSource = "HistoryAndPlugin"
-# 每次回溯输入历史，光标定位于输入内容末尾
-                HistorySearchCursorMovesToEnd = $true                        
-            }
+                if ( $(Test-CommandExists 'Set-PSReadLineOption') )
+                {                               
+				
+				
+
+	                if ( $(Get-Module PSReadline).Version -ge 2.2 ) {
+	                    # 设置预测文本来源为历史记录
+	                    Set-PSReadLineOption -predictionsource history -ea SilentlyContinue
+	                }
+
+		            $PSReadLineOptions = @{
+		                HistorySavePath = $global:historyPath
+					# 设置预测文本来源为历史记录
+		                PredictionSource = "HistoryAndPlugin"
+					# 每次回溯输入历史，光标定位于输入内容末尾
+		                HistorySearchCursorMovesToEnd = $true                        
+		            }
             
-            Set-PSReadLineOption @PSReadLineOptions
+		            Set-PSReadLineOption @PSReadLineOptions
             
-            echo ($host.Name -eq 'ConsoleHost')
-            # Set-PSReadLineOption -EditMode Emac
-            
+		            echo ($host.Name -eq 'ConsoleHost')
+		}
 # 设置 Tab 为菜单补全和 Intellisense
                 Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete       
 Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
@@ -41,24 +48,8 @@ Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
             # 设置向上键为后向搜索历史记录 # Autocompletion for arrow keys @ https://dev.to/ofhouse/add-a-bash-like-autocomplete-to-your-powershell-4257
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 
-# 设置向下键为前向搜索历史纪录
-
-
-            if ( $null -ne $(Get-Module PSFzf)  ) {
-                #Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-                #$FZF_COMPLETION_TRIGGER='...'
-                Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-            }
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
 #-------------------------------  Set Hot-keys END    -------------------------------
-
-
-                if ( $(Get-Module PSReadline).Version -ge 2.2 ) {
-                    # 设置预测文本来源为历史记录
-                    Set-PSReadLineOption -predictionsource history -ea SilentlyContinue
-                }
-
 
                 if ( $(Test-CommandExists 'Set-PSReadLineOption') )
                 {
--- functions.psm1
+++ functions.psm1
@@ -14,23 +14,22 @@
 # define these environment variables if not set already and also provide them as PSVariables
 
 
-#------------------------------- prompt beguin -------------------------------
-#src: https://stackoverflow.com/a/34098997/7595318
-	function Test-IsInteractive {
-    # Test each Arg for match of abbreviated '-NonInteractive' command.
-    $NonInteractiveFlag = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonInteractive' }
-    if ( (-not [Environment]::UserInteractive) -or ( $NonInteractiveFlag -ne $null ) ) {
-        return $false
-    }
-    return $true
-}
+	#------------------------------- prompt beguin -------------------------------
+	#src: https://stackoverflow.com/a/34098997/7595318
+		function Test-IsInteractive {
+	    # Test each Arg for match of abbreviated '-NonInteractive' command.
+	    $NonInteractiveFlag = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonInteractive' }
+	    if ( (-not [Environment]::UserInteractive) -or ( $NonInteractiveFlag -ne $null ) ) {
+	        return $false
+	    }
+	    return $true
+	}
 
 
 	function Test-IsAdmin { $user = [Security.Principal.WindowsIdentity]::GetCurrent(); return $(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator); }
 
 	function Reopen-here { Get-Process explorer | Stop-Process Start-Process "$(Get-HostExecutable)" -ArgumentList "-noProfile -noLogo -Command 'Get-Process explorer | Stop-Process'" -verb "runAs"}
 	function Test-Administrator                     { $user = [Security.Principal.WindowsIdentity]::GetCurrent() ; (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) } # https://community.spiceworks.com/topic/1570654-what-s-in-your-powershell-profile?page=1#entry-5746422
-	function Get-DefaultAliases                     { Get-Alias | Where-Object                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        { $_.Options -match "ReadOnly" }}
 	function get-envVar                             { Get-Childitem -Path Env:*}
 	function get-historyPath                        { (Get-PSReadlineOption).HistorySavePath }
 	function get-parameters                         { Get-Member -Parameter *}
@@ -38,66 +37,14 @@
 	function man                                    { Get-Help $args[0] | out-host -paging }
 	function My-Scripts                             { Get-Command -CommandType externalscript }
 	function open-ProfileFolder                     { explorer (split-path -path $profile -parent)}
-	function read-aliases 				            { Get-Alias | Where-Object { $_.Options -notmatch "ReadOnly" }}
 	function read-EnvPaths	 			            { ($Env:Path).Split(";") }
 	function sort-PathByLvl                         { param( $inputList) $inputList                                                                                                                                                                                                                                          | Sort                                                                                                                                                                                                                                                                                                                                                                                   {($_ -split '\\').Count},                                                                                                                                                                                                                                                                                            {$_} -Descending                                       | select -object -first 2                                          | %                                                                                                                                                                                                                                                                                                                                                           { $error.clear()                                            ; try                                                                                                                                                                                                { out -null -input (test -ModuleManifest $_ > '&2>&1' ) } catch                                                                               { "Error" } ; if (!$error) { $_ } }}
 	function which($name) 				            { Get-Command $name | Select-Object -ExpandProperty Definition } #should use more
 
 function sanitize-clipboard { $regex = "[^a-zA-Z0-9"+ "\$\#^\\|&.~<>@:+*_\(\)\[\]\{\}?!\t\s\['" + '=åäöÅÄÖ"-]'  ; $original = Get-clipboard ; $sanitized = $original -replace $regex,'' ; $sanitized | set-clipboard }
 
-function Translate-Path {
-  [CmdletBinding()]
-  param (
-    # The relative path
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string]
-    $RelativePath,
 
-    # The base directory`
-    [Parameter(Mandatory = $true)]
-    [ValidateScript({Test-Path $_ -PathType Container})]
-    [string]
-    $BaseDirectory
-  )
 
-  # Split the relative path by the "/" character`
-  $PathSegments = $RelativePath.Split("/")
-
-  # Set the current directory to the base directory`
-  $CurrentDirectory = $BaseDirectory
-
-  # Loop through each path segment`
-  foreach ($Segment in $PathSegments) {
-    # If the segment is "..", go up one level in the directory hierarchy`
-    if ($Segment -eq "..") {
-      $CurrentDirectory = Split-Path -Path $CurrentDirectory -Parent
-    }
-    # If the segment is ".git", stop the loop and append the rest of the path`
-    elseif ($Segment -eq ".git") {
-      break
-    }
-    # Otherwise, ignore the segment`
-    else {
-      continue
-    }
-  }
-
-  # Get the index of the ".git" segment in the path segments`
-  $GitIndex = [array]::IndexOf($PathSegments, ".git")
-
-  # Get the rest of the path segments after the ".git" segment`
-  $RestOfPath = $PathSegments[($GitIndex)..$PathSegments.Length]
-
-  # Join the rest of the path segments by the "/" character`
-  $RestOfPath = $RestOfPath -join "/"
-
-  # Append the rest of the path to the current directory`
-  $AbsolutePath = Join-Path -Path $CurrentDirectory -ChildPath $RestOfPath
-
-  # Return the absolute path as a string`
-  return "$AbsolutePath"
-}
-}
 function Set-ErrorView {
     # Use the parameter attribute to make it mandatory and validate the input
     param(
@@ -108,7 +55,7 @@ function Set-ErrorView {
 
     # Set the ErrorView variable to the specified value
     $global:ErrorView = $ErrorView
-}
+}	    # Set the ErrorView variable to the specified value
 function read-EnvPaths                          { ($Env:Path).Split(";") }
 function read-uptime                            { Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime'; EXPRESSION=                                                                                                                                                                                                                                                                                 {$_.ConverttoDateTime($_.lastbootuptime)}} } #doesn't psreadline module implement this already?
 
