82364c2a7e0783a3f446d6f7e2c2cd976b930a59
perXautomatik <christoffer.broback@gmail.com>
2023-12-21 13:30:09 +0100

    state

--- profile.ps1
+++ profile.ps1
@@ -1,107 +1,47 @@
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# src: https://gist.github.com/apfelchips/62a71500a0f044477698da71634ab87b
-# New-Item $(Split-Path "$($PROFILE.CurrentUserCurrentHost)") -ItemType Directory -ea 0; Invoke-WebRequest -Uri "https://git.io/JYZTu" -OutFile "$($PROFILE.CurrentUserCurrentHost)"
+#Requires -Version 7
 
-# ref: https://devblogs.microsoft.com/powershell/optimizing-your-profile/#measure-script
-# ref: Powershell $? https://stackoverflow.com/a/55362991
+# Version 1.2.10
 
-# ref: Write-* https://stackoverflow.com/a/38527767
-# Write-Host wrapper for Write-Information -InformationAction Continue
-# define these environment variables if not set already and also provide them as PSVariables
-
-if ( ( $null -eq $PSVersionTable.PSEdition) -or ($PSVersionTable.PSEdition -eq "Desktop") ) { $PSVersionTable.PSEdition = "Desktop" ;$IsWindows = $true }
-
-if ( -not $IsWindows ) { function Test-IsAdmin { if ( (id -u) -eq 0 ) { return $true } return $false } }
-#src: https://devblogs.microsoft.com/scripting/use-a-powershell-function-to-see-if-a-command-exists/
-function Test-CommandExists {
-    Param ($command)
-    $oldErrorActionPreference = $ErrorActionPreference
-    $ErrorActionPreference = 'stop'
-    try { Get-Command $command; return $true }
-    catch {return $false}
-    finally { $ErrorActionPreference=$oldErrorActionPreference }
-}    
-
-function Clean-Object {
-    process {
-        $_.PSObject.Properties.Remove('PSComputerName')
-        $_.PSObject.Properties.Remove('RunspaceId')
-        $_.PSObject.Properties.Remove('PSShowComputerName')
-    }
-    #Where-Object { $_.PSObject.Properties.Value -ne $null}
-}
-
-function Get-Environment {  # Get-Variable to show all Powershell Variables accessible via $
-    if ( $args.Count -eq 0 ) {
-        Get-Childitem env:
-    } elseif( $args.Count -eq 1 ) {
-        Start-Process (Get-Command $args[0]).Source
-    } else {
-        Start-Process (Get-Command $args[0]).Source -ArgumentList $args[1..($args.Count-1)]
-    }
-}
-
-function cf {
-    if ( $null -ne $(Get-Module PSFzf) ) {
-        Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
-    } else {
-        Write-Error "please install PSFzf"
-    }
-}
-
-#-------------------------------   Set alias BEGIN    -------------------------------
-$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
-
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$aliasPath = ($profile | Split-Path -Parent)+'\profileAliases.ps1'
-Add-Content -Path $Profile -Value (Get-Content $aliasPath)
-$sw.Stop()
-"loaded aliases"
-$sw.Elapsed.TotalMilliseconds
-
-
-#-------------------------------    Set alias END     -------------------------------
-
-Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
-Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
-Write-Host "Profile:   $PSCommandPath"
-
+# check if newer version
 <#
  * FileName: Microsoft.PowerShell_profile.ps1
  * Author: perXautomatik
  * Email: christoffer.broback@gmail.com
  * Date: 08/03/2022
- * Copyright: No copyright. You can use this code for anything with no warranty.
-#>
+ * Copyright: No copyright. You can use this code for anything with no warranty. 
+ #>
 
-#loadMessage
-echo "Microsoft.PowerShell_profile.ps1"
+
+if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
+# $0: %UserProfile%\Documents\PowerShell\Profile.ps1 # for PS-Core
+# src:
+$Profile.CurrentUserCurrentHost = $PSCommandPath # this file is my Profile
+$global:profile_initialized = $false
+
+$gistUrl = "https://api.github.com/gists/a208d2bd924691bae7ec7904cab0bd8e"
+$latestVersionFile = [System.IO.Path]::Combine("$HOME",'.latest_profile_version')
+$versionRegEx = "# Version (?<version>\d+\.\d+\.\d+)"
+
 
 # Increase history
 $MaximumHistoryCount = 10000
 
+  $global:LASTEXITCODE = $currentLastExitCode
 
-#src: https://stackoverflow.com/a/34098997/7595318
-function Test-IsInteractive {
-    # Test each Arg for match of abbreviated '-NonInteractive' command.
-    $NonInteractiveFlag = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonInteractive' }
-    if ( (-not [Environment]::UserInteractive) -or ( $NonInteractiveFlag -ne $null ) ) {
-        return $false
+function timer($script,$message){
+    $t = [system.diagnostics.stopwatch]::startnew()
+    $job = Start-ThreadJob -ScriptBlock $script
+    
+    while($job.state -ne "Completed"){    
+        Write-Output = "$message Elapsed: $($t.elapsed) "                
+        start-sleep 1
     }
-    return $true
+    $t.stop()
+    $job | Receive-Job
 }
 
-#if ( Test-IsInteractive )  { 	(preferably use -noLogo) } # Clear-Host # remove advertisements 
-
-
-function Download-Latest-Profile {
-    New-Item $( Split-Path $($PROFILE.CurrentUserCurrentHost) ) -ItemType Directory -ea 0
-    if ( $(Get-Content "$($PROFILE.CurrentUserCurrentHost)" | Select-String "62a71500a0f044477698da71634ab87b" | Out-String) -eq "" ) {
-        Move-Item -Path "$($PROFILE.CurrentUserCurrentHost)" -Destination "$($PROFILE.CurrentUserCurrentHost).bak"
-    }
-    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/apfelchips/62a71500a0f044477698da71634ab87b/raw/Profile.ps1" -OutFile "$($PROFILE.CurrentUserCurrentHost)"
-    Reload-Profile
-}
 
 # Produce UTF-8 by default
 
@@ -111,76 +51,92 @@ if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
 	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
 }	
 
-#------------------------------- Set Paths           -------------------------------
+$profileFolder = (split-path $PROFILE -Parent)
 
-$paths = join-path -Path (split-path $profile -Parent)  -ChildPath 'setPaths.ps1'
+#------------------------------- check online for profileUpdates BEGIN -------------------------------
+# downloads and set version numbers
+& "$PSScriptRoot\profileImport.ps1";
 
-Import-Module  $paths
-#------------------------------- Set Paths  end       -------------------------------
+#------------------------------- check online for profileUpdates END   -------------------------------
 
-#------------------------------- Import Modules BEGIN -------------------------------
-
-$profileFolder = (split-path $profile -Parent)
-$pos = join-path -Path $profileFolder -ChildPath 'importModules.ps1'
- Import-Module $pos
-#------------------------------- Import Modules END   -------------------------------
-
-
+#------------------------------- Import updateTypeData BEGIN -------------------------------
+Update-TypeData "$PSScriptRoot\My.Types.Ps1xml"
+#------------------------------- Import updateTypeData END   -------------------------------
 #------------------------------- overloading begin
-
-#https://www.sapien.com/blog/2014/10/21/a-better-tostring-method-for-hash-tables/
-
-#better hashtable ToString method
-  Update-TypeData -TypeName "System.Collections.HashTable"   `
-  -MemberType ScriptMethod `
-  -MemberName "ToString" -Value { $hashstr = "@{"; $keys = $this.keys; foreach ($key in $keys) { $v = $this[$key];
-	     if ($key -match "\s") { $hashstr += "`"$key`"" + "=" + "`"$v`"" + ";" }
-	     else { $hashstr += $key + "=" + "`"$v`"" + ";" } }; $hashstr += "}";
-	     return $hashstr }
+& "$PSScriptRoot\RO_betterToStringHashMap.ps1";
 #-------------------------------  overloading end
 
-#------------------------------- SystemMigration      -------------------------------
 
-#choco check if installed
-#path to list of aps to install
-#choco ask to install if not present
+#------------------------------- Import Modules BEGIN -------------------------------
+$pos = ($profileFolder+'\importModules.psm1');
+Import-Module -name $pos  -Scope Global -PassThru
+Import-MyModules; echo "modules imported"
+#------------------------------- Import Modules END   -------------------------------
 
-#list of portable apps,download source
-#path
-#download and extract if not present, ask to confirm
+#------------------------------- Import EverythingModules BEGIN -------------------------------
+$pos = ($profileFolder+'\EverythingHelpers.psm1');
+Import-Module -name $pos  -Scope Global -PassThru
+#------------------------------- Import EverythingModules BEGIN  -------------------------------
 
-#path to portable apps
-#path to standard download location
+#------------------------------- Import GitHelpers BEGIN -------------------------------
+$pos = ($profileFolder+'\GitHelpers.psm1');
+Import-Module -name $pos  -Scope Global -PassThru
+#------------------------------- Import GitHelpers BEGIN  -------------------------------
+
+#------------------------------- Import HelperFunctions BEGIN -------------------------------
+$pos = ($profileFolder+'\functions.psm1')
+Import-Module -name $pos  -Scope Global -PassThru
+#------------------------------- Import HelperFunctions END   -------------------------------
+
+#------------------------------- Import sqlite BEGIN -------------------------------
+$pos = ($profileFolder+'\sqlite.psm1')
+Import-Module -name $pos  -Scope Global -PassThru
+#------------------------------- Import sqlite END   -------------------------------
 
 
-#git Repos paths and origions,
-#git systemwide profile folder
-#git global path
 
-#everything data folder
-#autohotkey script to run on startup
-
-#startup programs
-
-#reg to add if not present
-
-#------------------------------- SystemMigration end  -------------------------------
-
-#------------------------------- Styling begin --------------------------------------					      
-#change selection to neongreen
-#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
-$colors = @{
-   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
+function destroyProfile
+{
+    Set-Content -Path $PROFILE -Value ''
 }
-Set-PSReadLineOption -Colors $colors
+function rebuildProfile
+{
+    
+    #------------------------------- Cache Paths           ------------------------------- # creates path cache, if not pressent, expect other methods to destroy cache case of false paths. # path file should be simpler to parse than to calling everything
+    $varpath  = ($profileFolder+'\setPaths.psm1');
+    timer -message 'adding paths' -script {Add-Content -Path $using:PROFILE -Value (Get-Content $using:varpath)}
+    #------------------------------- Cache Paths  end       -------------------------------
 
-# Style default PowerShell Console
-$shell = $Host.UI.RawUI
+    #-------------------------------   Set Variables BEGIN    -------------------------------
+    $varPath = ($profileFolder+'\setVariables.ps1'); 
+    timer -message 'adding variables' -script {Add-Content -Path $using:PROFILE -Value (Get-Content $using:varpath)}
+    #-------------------------------    Set Variables END     -------------------------------
 
-$shell.WindowTitle= "PS"
+    #-------------------------------   Set alias BEGIN    -------------------------------
+    $aliasPath =($profileFolder+'\profileAliases.ps1') ;  $TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType) ;
+    timer -message "adding aliases" -script { Add-Content -Path $using:PROFILE -Value (Get-Content $using:aliasPath) } 
+    #-------------------------------    Set alias END     -------------------------------
 
-$shell.BackgroundColor = "Black"
-$shell.ForegroundColor = "White"
+    #------------------------------- Console BEGIN -------------------------------
+    $aliasPath =($profileFolder+'\prompt.ps1') ; 
+    timer -message "import console" -script {Add-Content -Path $using:Profile -Value (Get-Content $using:aliasPath) } 
+    #------------------------------- Console END   -------------------------------
 
-# Load custom theme for Windows Terminal
-#Set-Theme LazyAdmin
+    #------------------------------- Console BEGIN -------------------------------
+    $aliasPath =($profileFolder+'\PsReadLineIntial.ps1') ; 
+    timer -message "PsReadLine Intial" -script {Add-Content -Path $using:Profile -Value (Get-Content $using:aliasPath) } 
+    #------------------------------- Console END   -------------------------------
+    
+}
+
+
+if (( $error | ?{ $_ -match 'everything' } ).length -gt 0)
+{
+    $everythingError = $true
+}
+
+if (( $error | ?{ $_ -match 'sqlite' } ).length     -gt 0)
+{
+    $sqliteError = $true
+}
+
