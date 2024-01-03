e0fc2addaa23afd7674200ab3996378308cde57f
perXautomatik <christoffer.broback@gmail.com>
2024-01-03 02:55:34 +0100

    state

--- GitHelpers.psm1
+++ GitHelpers.psm1
@@ -1,12 +1,21 @@
 
 if ( $(Test-CommandExists 'git') ) {
     Set-Alias g    git -Option AllScope
-    function git-root { $gitrootdir = (git rev-parse --show-toplevel) ; if ( $gitrootdir ) { Set-Location $gitrootdir } }
+
+    function git-root {
+        $gitrootdir = (git rev-parse --show-toplevel)
+        if ( $gitrootdir ) {
+            Set-Location $gitrootdir
+        }
+    }
 
     if ( $IsWindows ) {
         function git-sh {
-            if ( $args.Count -eq 0 ) { . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\sh") -l
-            } else { . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\sh") $args }
+            if ( $args.Count -eq 0 ) {
+                . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\sh") -l
+            } else {
+                . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\sh") $args
+            }
         }
 
         function git-bash {
@@ -17,15 +26,25 @@ if ( $(Test-CommandExists 'git') ) {
             }
         }
 
-        function git-vim { . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\bash") -l -c `'vim $args`' }
+        function git-vim {
+           . $(Join-Path -Path $(Split-Path -Path $(Get-Command git).Source) -ChildPath "..\bin\bash") -l -c `'vim $args`'
+        }
 
-        if ( -Not (Test-CommandExists 'sh') ){ Set-Alias sh   git-sh -Option AllScope }
+        if ( -Not (Test-CommandExists 'sh') ){
+            Set-Alias sh   git-sh -Option AllScope
+        }
 
-        if ( -Not (Test-CommandExists 'bash') ){ Set-Alias bash   git-bash -Option AllScope }
+        if ( -Not (Test-CommandExists 'bash') ){
+            Set-Alias bash   git-bash -Option AllScope
+        }
 
-        if ( -Not (Test-CommandExists 'vi') ){ Set-Alias vi   git-vim -Option AllScope }
+        if ( -Not (Test-CommandExists 'vi') ){
+            Set-Alias vi   git-vim -Option AllScope
+        }
 
-        if ( -Not (Test-CommandExists 'vim') ){ Set-Alias vim   git-vim -Option AllScope }
+        if ( -Not (Test-CommandExists 'vim') ){
+            Set-Alias vim   git-vim -Option AllScope
+        }
     }
 
 
@@ -36,3 +55,15 @@ set-alias GitAdEPathAsSNB    invoke-GitSubmoduleAdd                 -Option AllS
 set-alias GitUp              invoke-GitLazy                         -Option AllScope
     set-alias -Name:"remote" -Value:"invoke-gitRemote" -Description:"" -Option:"AllScope"
 }
+
+    function stree($directory = $pwd) {
+        $gitrootdir = (Invoke-Command{Set-Location $args[0]; git rev-parse --show-toplevel 2>&1;} -ArgumentList $directory)
+
+        if ( Test-Path -Path "$gitrootdir\.git" -PathType Container) {
+            $newestExe = Get-Item "${env:ProgramFiles(x86)}\Atlassian\SourceTree\SourceTree.exe" | select -Last 1
+            Write-Debug "Opening $gitrootdir with $newestExe"
+            Start-Process -filepath $newestExe -ArgumentList "-f `"$gitrootdir`" log"
+        } else {
+            Write-Error "git directory not found"
+        }
+    }
