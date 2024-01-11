5e66883749e60992af76e8d48af91f50beebca59
perXautomatik <christoffer.broback@gmail.com>
2024-01-11 10:36:43 +0100

    standardized loading methods

--- profile.ps1
+++ profile.ps1
@@ -15,17 +15,20 @@ Import-Module -name $pos   -DisableNameChecking -PassThru
 Import-Module "$PSScriptRoot/helpers.psm1" -DisableNameChecking -PassThru
 
 if ( Test-IsInteractive )  { 	
-# (preferably use -noLogo)
-Clear-Host # remove advertisements 
+	# (preferably use -noLogo)
+	Clear-Host # remove advertisements 
 } 
 
-# Source scripts
-. "$PSScriptRoot/internal/functions.ps1"
-. "$PSScriptRoot/internal/env.ps1"
-. "$PSScriptRoot/internal/aliases.ps1"
-. "$PSScriptRoot/internal/autocompletions.ps1"
+Load-AllChildPs1 -path  "$PSScriptRoot/internal/" -exclude "cd.ps1","lf_icons.ps1","profileImport.ps1","prompt.ps1","setVariables.ps1","standardFunctionOverride.ps1","key-config.ps1"
 
-Import-Module "$PSScriptRoot/launch.psm1" -DisableNameChecking
+Import-PriorityModule -Directory "$PSScriptRoot\internal" -PriorityOrder @{ 
+	"importModules.psm1" = 1; 
+	"EverythingHelpers.psm1" = 2; 
+	"sqlite.psm1" = 3;
+	"GitHelpers.psm1" = 4; 
+}
+
+
 
 $gistUrl = "https://api.github.com/gists/a208d2bd924691bae7ec7904cab0bd8e"
 $latestVersionFile = [System.IO.Path]::Combine("$HOME",'.latest_profile_version')
@@ -39,7 +42,6 @@ Import-Module-Verified "PSReadLine"
 
 if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
 	# https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget
-	
 	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
 }	
 
@@ -51,18 +53,9 @@ Update-TypeData "$PSScriptRoot\My.Types.Ps1xml"
 #------------------------------- overloading begin
 & "$PSScriptRoot\RO_betterToStringHashMap.ps1";
 #------------------------------- Import Modules BEGIN -------------------------------
-$profileFolder = $PSScriptRoot
 
-#Import-MyModules; echo "modules imported"
-#------------------------------- Import EverythingModules BEGIN -------------------------------
-$pos = ($profileFolder+'\internal\EverythingHelpers.psm1');
-Import-Module -name $pos  -Scope Global -PassThru
-#------------------------------- Import sqlite BEGIN -------------------------------
-$pos = ($profileFolder+'\internal\sqlite.psm1')
-Import-Module -name $pos  -Scope Global -PassThru
-
-. "$PSScriptRoot/internal/loads.ps1"
 . "$PSScriptRoot/internal/key-config.ps1"
+$profileFolder = $PSScriptRoot
 
 Import-Module-Verified "cd-extras" #Load-Module "cd-extras"  
 
@@ -78,8 +71,6 @@ function destroyProfile
 
 function rebuildProfile
 {
-    $varpath  = ($profileFolder+'\internal\setPaths.psm1');
-    timer -message 'adding paths' -script {Add-Content -Path $using:PROFILE -Value (Get-Content $using:varpath)}
     #-------------------------------   Set Variables BEGIN    -------------------------------
     $varPath = ($profileFolder+'\internal\setVariables.ps1'); 
     timer -message 'adding variables' -script {Add-Content -Path $using:PROFILE -Value (Get-Content $using:varpath)}
