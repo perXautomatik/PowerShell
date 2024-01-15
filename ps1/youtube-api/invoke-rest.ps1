c029af9cb71a3ea1f45722d43708dec676c8b6bd
perXautomatik <christoffer.broback@gmail.com>
2024-01-15 12:42:33 +0100

    can i in same fashion get statistics from my playlist, i'd like to get a list of creators and number of video occurences in that playlist

--- ps1/youtube-api/invoke-rest.ps1
+++ ps1/youtube-api/invoke-rest.ps1
@@ -1,43 +1,105 @@
+Yes, you can get statistics from your playlist using PowerShell and the YouTube Data API. You can also get a list of creators and the number of videos they have in your playlist. However, you will need to modify the script I provided earlier to include some additional steps.
+
+First, you will need to get the channel ID of each video in your playlist. You can do this by adding the `contentDetails` part to the `playlistItems.list` request, and selecting the `videoOwnerChannelId` property from the response. For example:
+
+```powershell
 $playlistId = "PLBCF2DAC6FFB574DE" # replace with your playlist ID
 $creatorName = "Google Developers" # replace with the creator name or channel ID
 $apiKey = "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY" # replace with your API key
 $baseUrl = "https://www.googleapis.com/youtube/v3"
 $playlistItemsUrl = "$baseUrl/playlistItems"
 
-# get the video IDs of the videos in the playlist by the creator
-$response = Invoke-RestMethod -Uri $playlistItemsUrl -Method Get -ContentType "application/json" -Query @{
-    part = "snippet"
-    playlistId = $playlistId
-    filter = $creatorName
-    key = $apiKey
-    maxResults = 50
+$videoIds = @() # create an empty array to store the video IDs
+$channelIds = @() # create an empty array to store the channel IDs
+$nextPageToken = $null # initialize the next page token to null
+
+while ($true) {
+    # if there is no next page token, get the first page of results
+    if ($nextPageToken -eq $null) {
+        $response = Invoke-RestMethod -Uri $playlistItemsUrl -Method Get -ContentType "application/json" -Query @{
+            part = "snippet,contentDetails"
+            playlistId = $playlistId
+            filter = $creatorName
+            key = $apiKey
+            maxResults = 50
+        }
+    }
+    # otherwise, get the next page of results using the next page token
+    else {
+        $response = Invoke-RestMethod -Uri $playlistItemsUrl -Method Get -ContentType "application/json" -Query @{
+            part = "snippet,contentDetails"
+            playlistId = $playlistId
+            filter = $creatorName
+            key = $apiKey
+            maxResults = 50
+            pageToken = $nextPageToken
+        }
+    }
+
+    # add the video IDs and channel IDs from the current page to the arrays
+    $videoIds += $response.items.snippet.resourceId.videoId
+    $channelIds += $response.items.contentDetails.videoOwnerChannelId
+
+    # update the next page token from the response
+    $nextPageToken = $response.nextPageToken
+
+    # if there is no next page token, break the loop
+    if ($nextPageToken -eq $null) {
+        break
+    }
 }
+```
 
-$videoIds = $response.items.snippet.resourceId.videoId # this will store the video IDs in an array
+Next, you will need to get the channel title of each channel ID. You can do this by using the `channels.list` request, and selecting the `title` property from the response. For example:
 
-# construct the batch request body
-$boundary = "batch_youtube_example" # this can be any string
-$body = "" # this will store the request body
+```powershell
+$channelsUrl = "$baseUrl/channels"
 
-foreach ($videoId in $videoIds) {
-    # add a boundary line
-    $body += "--$boundary`r`n"
-    # add the content type header
-    $body += "Content-Type: application/http`r`n"
-    # add the content transfer encoding header
-    $body += "Content-Transfer-Encoding: binary`r`n"
-    # add a blank line
-    $body += "`r`n"
-    # add the individual request method, URL, and headers
-    $body += "DELETE $playlistItemsUrl?id=$videoId&key=$apiKey`r`n"
-    $body += "Content-Type: application/json`r`n"
-    # add another blank line
-    $body += "`r`n"
+$channelTitles = @() # create an empty array to store the channel titles
+
+foreach ($channelId in $channelIds) {
+    $response = Invoke-RestMethod -Uri $channelsUrl -Method Get -ContentType "application/json" -Query @{
+        part = "snippet"
+        id = $channelId
+        key = $apiKey
+    }
+
+    # add the channel title from the response to the array
+    $channelTitles += $response.items.snippet.title
 }
+```
 
-# add the final boundary line
-$body += "--$boundary--"
+Finally, you will need to count the number of occurrences of each channel title in your playlist. You can do this by using a hashtable to store the channel title as the key and the count as the value. For example:
 
-# send the batch request
-$response = Invoke-RestMethod -Uri "$playlistItemsUrl?batch=true" -Method Post -ContentType "multipart/mixed; boundary=$boundary" -Body $body
+```powershell
+$channelCounts = @{} # create an empty hashtable to store the channel counts
+
+foreach ($channelTitle in $channelTitles) {
+    # if the channel title already exists in the hashtable, increment the count by one
+    if ($channelCounts.ContainsKey($channelTitle)) {
+        $channelCounts[$channelTitle]++
+    }
+    # otherwise, add the channel title to the hashtable with a count of one
+    else {
+        $channelCounts[$channelTitle] = 1
+    }
+}
+```
+
+Now you have a hashtable that contains the channel title and the number of videos they have in your playlist. You can display the results in a table format by using the `Format-Table` cmdlet. For example:
+
+```powershell
+$channelCounts | Format-Table -AutoSize
+```
+
+This will output something like this:
+
+```
+Name                           Value
+----                           -----
+Google Developers              50
+Android Developers             25
+Firebase                       15
+TensorFlow                     10
+```
 
