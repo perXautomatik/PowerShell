8be7645bc96437ff00dabb014348a79a5d4608bc
perXautomatik <christoffer.broback@gmail.com>
2024-01-15 12:14:09 +0100

    can you modify it so that 'F:' is a parameter but with a standard value non mandatory

--- ps1/invoke-operalauncher.ps1
+++ ps1/invoke-operalauncher.ps1
@@ -3,13 +3,16 @@ function Invoke-OperaLauncher {
   param (
     [Parameter(Mandatory, Position = 0)]
     [string]
-    $q
+    $q,
+    [Parameter(Position = 1)]
+    [string]
+    $DriveLetter = 'F:'
   )
 
-  Write-Verbose "Invoking OperaLauncher with parameter $q"
+  Write-Verbose "Invoking OperaLauncher with parameter $q on drive $DriveLetter"
 
   if ($PSCmdlet.ShouldProcess("OperaLauncher", "Invoke")) {
-    Invoke-Expression "F:; Set-Location F:\; .\OperaLauncher\opera.ps1 -a $q; & setFileExtension"
+    Invoke-Expression "$DriveLetter; Set-Location $DriveLetter\; .\OperaLauncher\opera.ps1 -a $q; & setFileExtension"
   }
 }
 
