6d97085f7ecf9b9aef15b197355dde428c5ef7f7
Christoffer Brobäck <christoffer.broback@gmail.com>
2024-01-19 14:51:43 +0100

    reorganized, and made sure code duplication does not occure between document instance

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,63 +13,49 @@
     such as customizing the ISE editor or adding ISE-specific functions.
 #>
 
-# Increase history
-$MaximumHistoryCount = 10000
-
-# Produce UTF-8 by default
-
-if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
-	# https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget
-	
-	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
-}	
-
 #-------------------------------    Functions END     -------------------------------
 $TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
 if (test-path alias:\cd)              { remove-item -force alias:\cd }                 # We override with cd.ps1
 if (test-path alias:\chdir)           { remove-item -force alias:\chdir }              # We override with an alias to cd.ps1
+
 if (test-path function:\prompt)       { remove-item -force function:\prompt }          # We override with prompt.ps1
-                Set-Alias history           	Get-History                           	-Option AllScope
-                Set-Alias kill              	killx                          			-Option AllScope
-                Set-Alias mv                	Move-Item                             	-Option AllScope
-                Set-Alias pwd               	Get-Location                          	-Option AllScope
-                Set-Alias rm                	Remove-Item                           	-Option AllScope
-                Set-Alias echo              	Write-Output                          	-Option AllScope
-                Set-Alias cls               	Clear-Host                            	-Option AllScope
-                Set-Alias copy              	Copy-Item                             	-Option AllScope
-                Set-Alias del               	Remove-Item                           	-Option AllScope
-                Set-Alias dir               	Get-Childitem                         	-Option AllScope
-                Set-Alias type              	Get-Content                           	-Option AllScope
-                Set-Alias sudo                  Elevate-Process           	            -Option AllScope
-                set-alias pastDoEdit        	find-historyAppendClipboard           	-Option AllScope
-                set-alias pastDo            	find-historyInvoke                    	-Option AllScope
-                set-alias everything        	invoke-Everything                     	-Option AllScope
-                set-alias executeThis       	invoke-FuzzyWithEverything            	-Option AllScope
-                set-alias exp-pro           	open-ProfileFolder                    	-Option AllScope
-                set-alias MyAliases         	read-aliases                          	-Option AllScope                
-                set-alias printpaths        	read-EnvPaths                         	-Option AllScope
-                set-alias uptime            	read-uptime                           	-Option AllScope
-                set-alias parameters        	get-parameters                        	-Option AllScope
-                set-alias accelerators      	([accelerators]::Get)                 	-Option AllScope
-                set-alias reboot            	exit-Nrenter                          	-Option AllScope
-                set-alias reload            	initialize-profile                    	-Option AllScope
-$profileFolder = (split-path $profile -Parent)
-Update-TypeData (join-path $profileFolder "My.Types.ps1xml")
-
-
 # Sometimes home doesn't get properly set for pre-Vista LUA-style elevated admins
- if ($home -eq "") { remove-item -force variable:\home $home = (get-content env:\USERPROFILE) (get-psprovider 'FileSystem').Home = $home } set-content env:\HOME $home
+if ($home -eq "") { 
+    remove-item -force variable:\home 
+    $home = (get-content env:\USERPROFILE) 
+    (get-psprovider 'FileSystem').Home = $home 
+}
+    set-content env:\HOME $home
+
+    Set-Alias history           	Get-History                           	-Option AllScope
+    Set-Alias kill              	killx                          			-Option AllScope
+    Set-Alias mv                	Move-Item                             	-Option AllScope
+    Set-Alias pwd               	Get-Location                          	-Option AllScope
+    Set-Alias rm                	Remove-Item                           	-Option AllScope
+    Set-Alias echo              	Write-Output                          	-Option AllScope
+    Set-Alias cls               	Clear-Host                            	-Option AllScope
+    Set-Alias copy              	Copy-Item                             	-Option AllScope
+    Set-Alias del               	Remove-Item                           	-Option AllScope
+    Set-Alias dir               	Get-Childitem                         	-Option AllScope
+    Set-Alias type              	Get-Content                           	-Option AllScope
+    Set-Alias sudo                  Elevate-Process           	            -Option AllScope
+    set-alias pastDoEdit        	find-historyAppendClipboard           	-Option AllScope
+    set-alias pastDo            	find-historyInvoke                    	-Option AllScope
+    set-alias everything        	invoke-Everything                     	-Option AllScope
+    set-alias executeThis       	invoke-FuzzyWithEverything            	-Option AllScope
+    set-alias exp-pro           	open-ProfileFolder                    	-Option AllScope
+    set-alias MyAliases         	read-aliases                          	-Option AllScope                
+    set-alias printpaths        	read-EnvPaths                         	-Option AllScope
+    set-alias uptime            	read-uptime                           	-Option AllScope
+    set-alias parameters        	get-parameters                        	-Option AllScope
+    set-alias accelerators      	([accelerators]::Get)                 	-Option AllScope
+    set-alias reboot            	exit-Nrenter                          	-Option AllScope
+    set-alias reload            	initialize-profile                    	-Option AllScope
 
 
-#loadMessage
-echo (Split-Path -leaf $MyInvocation.MyCommand.Definition)
-
-Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
-Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
-Write-Host ("Profile:   " + (Split-Path -leaf $MyInvocation.MyCommand.Definition))
-
-Write-Host "This script was invoked by: "+$($MyInvocation.Line)
-
+Update-TypeData (join-path $profileFolder "My.Types.ps1xml")
+# Increase history
+$MaximumHistoryCount = 10000
 
 #------------------------------- Styling begin --------------------------------------					      
 #change selection to neongreen
@@ -91,81 +77,3 @@ $shell = $Host.UI.RawUI
 #Set-Theme LazyAdmin
 
 
-function Ensure-Path {
-    param (
-        [string]$Path
-    )
-    # Validate the parameter
-    if (-not $Path) {
-        Write-Error "Path parameter is required"
-        return
-    }
-    # Check if the path is valid
-    if (-not [System.IO.Path]::IsPathRooted($Path)) {
-        # The path is relative, resolve it to an absolute path
-        $Path = Join-Path -Path (Get-Location) -ChildPath $Path
-    }
-    # Check if the path contains invalid characters
-    if ([System.IO.Path]::GetInvalidPathChars() -join '' -match [regex]::Escape($Path)) {
-        # The path contains invalid characters, throw an error
-        throw "The path '$Path' contains invalid characters."
-    }
-    # Check if the path exists
-    if (Test-Path -Path $Path) {
-        # The path exists, return it
-        return $Path
-    }
-    else {
-        # The path does not exist, try to create it
-        try {
-            $item = New-Item -Path $Path -ItemType Directory -Force -ErrorAction Stop
-            # Return the full path of the created directory
-            return $item.FullName
-        }
-        catch {
-            # An error occurred while creating the path, throw an error
-            throw "Failed to create the path '$Path': $($_.Exception.Message)"
-        }
-    }
-}
-function Invoke-Git {
-    param(
-        [Parameter(Mandatory=$false)]
-        [string]$Command = "status" # The git command to run
-    )
-
-    if ($Command -eq "") {
-        $Command = "status"
-    } elseif ($Command.StartsWith("git ")) {
-        $Command = $Command.Substring(4)
-    }
-
-    # Run the command and capture the output
-    $output = Invoke-Expression -Command "git $Command 2>&1" -ErrorAction Stop 
-
-    # Check the exit code and throw an exception if not zero
-    if ($LASTEXITCODE -ne 0) {
-        $errorMessage = $Error[0].Exception.Message
-        throw "Git command failed: git $Command. Error message: $errorMessage"
-    }
-
-    # return the output to the host
-    $output
-}
-function Spotify-UrlToPlaylist { $original = get-clipboard ; $transformed = $original.replace(“https://open.spotify.com/playlist/”, “spotify:user:spotify:playlist:”).replace(“?si=”, “=”) ; ($transformed -split '=')[0] | set-clipboard ; "done" }
-function git-filter-folder
-   {
-      param(
-      $namex
-      )
-      $current = git branch --show-current;
-      $branchName = ($namex+'b');
-      
-      git checkout -b $branchName
-      
-      git filter-repo --refs $branchName --subdirectory-filter $namex
-      
-      git checkout $current
-      
-      git filter-repo --refs $current --path $namex --invert-paths      
-   }
