b0adcab748ac01d4d46df57e21c2742a651e1f66
Christoffer Brobäck <christoffer.broback@gmail.com>
2024-01-19 14:54:35 +0100

    reorganized, made sure code duplication does not occure with .config instance improved prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
 <#
+ * FileName: Microsoft.PowerShell_profile.ps1
+ * Author: perXautomatik
+ * Email: christoffer.broback@gmail.com
+ * Copyright: No copyright. You can use this code for anything with no warranty.
     First, PowerShell will load the profile.ps1 file, which is the “Current User, All Hosts” profile.
     This profile applies to all PowerShell hosts for the current user, such as the console host or the ISE host. 
     You can use this file to define settings and commands that you want to use in any PowerShell session, regardless of the host.
@@ -9,74 +13,45 @@
     such as customizing the ISE editor or adding ISE-specific functions.
 #>
 
-#-------------------------------    Functions END     -------------------------------
-$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
-if (test-path alias:\cd)              { remove-item -force alias:\cd }                 # We override with cd.ps1
-if (test-path alias:\chdir)           { remove-item -force alias:\chdir }              # We override with an alias to cd.ps1
-if (test-path function:\prompt)       { remove-item -force function:\prompt }          # We override with prompt.ps1
-                Set-Alias history           	Get-History                           	-Option AllScope
-                Set-Alias kill              	killx                          			-Option AllScope
-                Set-Alias mv                	Move-Item                             	-Option AllScope
-                Set-Alias pwd               	Get-Location                          	-Option AllScope
-                Set-Alias rm                	Remove-Item                           	-Option AllScope
-                Set-Alias echo              	Write-Output                          	-Option AllScope
-                Set-Alias cls               	Clear-Host                            	-Option AllScope
-                Set-Alias copy              	Copy-Item                             	-Option AllScope
-                Set-Alias del               	Remove-Item                           	-Option AllScope
-                Set-Alias dir               	Get-Childitem                         	-Option AllScope
-                Set-Alias type              	Get-Content                           	-Option AllScope
-                Set-Alias sudo                  Elevate-Process           	            -Option AllScope
-                set-alias pastDoEdit        	find-historyAppendClipboard           	-Option AllScope
-                set-alias pastDo            	find-historyInvoke                    	-Option AllScope
-                set-alias everything        	invoke-Everything                     	-Option AllScope
-                set-alias executeThis       	invoke-FuzzyWithEverything            	-Option AllScope
-                set-alias exp-pro           	open-ProfileFolder                    	-Option AllScope
-                set-alias MyAliases         	read-aliases                          	-Option AllScope                
-                set-alias printpaths        	read-EnvPaths                         	-Option AllScope
-                set-alias uptime            	read-uptime                           	-Option AllScope
-                set-alias parameters        	get-parameters                        	-Option AllScope
-                set-alias accelerators      	([accelerators]::Get)                 	-Option AllScope
-                set-alias reboot            	exit-Nrenter                          	-Option AllScope
-                set-alias reload            	initialize-profile                    	-Option AllScope
-$profileFolder = (split-path $profile -Parent)
+
+# Produce UTF-8 by default
+
+if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
+	# https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget
+	
+	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
+}	
+
+
 . $env:USERPROFILE\.config\WindowsPowerShell\profile.ps1
 
+$profileFolder = (split-path $profile -Parent)
 
 # Sometimes home doesn't get properly set for pre-Vista LUA-style elevated admins
  if ($home -eq "") { remove-item -force variable:\home $home = (get-content env:\USERPROFILE) (get-psprovider 'FileSystem').Home = $home } set-content env:\HOME $home
 
-#------------------------------- Styling begin --------------------------------------					      
-#change selection to neongreen
-#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
-$colors = @{
-   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
-}
-#Set-PSReadLineOption -Colors $colors
-
-# Style default PowerShell Console
-$shell = $Host.UI.RawUI
-
-$shell.WindowTitle= "PS"
-
-$shell.BackgroundColor = "Black"
-$shell.ForegroundColor = "White"
-
-# Load custom theme for Windows Terminal
-#Set-Theme LazyAdmin
-
-Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
 
 #loadMessage
-echo (Split-Path -leaf $MyInvocation.MyCommand.Definition)
-
-Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+#Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+$profilex = (Split-Path -leaf $MyInvocation.MyCommand.Definition);
+# Use a subexpression operator
+$inv = $($MyInvocation.Line)
 Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
-Write-Host ("Profile:   " + (Split-Path -leaf $MyInvocation.MyCommand.Definition))
+if(($inv -split "\'")[1] -ne (Join-Path -path "$profileFolder" -ChildPath "$profilex" ))
+{
+    Write-Host "This script was invoked by: $inv"
+}
+else
+{ 
+  Write-Host "This script was invoked by:"
+}
+Write-Host ("Profile:   " + $profilex)
+write-host ("Profile folder:  " + $profileFolder)
 
-Write-Host "This script was invoked by: "+$($MyInvocation.Line)
 
-#------------------------------- Styling end --------------------------------------
 
+
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
 
 function repairHead {param($from="refs/heads/master",$to="origin/master"); $expr = "git update-ref "+$from+" "+ $to; invoke-expression $expr }
 
