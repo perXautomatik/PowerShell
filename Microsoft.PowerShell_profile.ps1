9a31da210d3eec4f4705361ede29bf2868f3396b
Christoffer Brobäck <christoffer.broback@gmail.com>
2024-02-01 22:21:38 +0100

    Merge remote-tracking branch 'remotes/win-dot/Dotfiles' into UserFolder-config/cnfg-b
    Signed-off-by: Christoffer Brobäck <christoffer.broback@gmail.com>

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,171 @@
+<#
+ * FileName: Microsoft.PowerShell_profile.ps1
+ * Author: perXautomatik
+ * Email: christoffer.broback@gmail.com
+ * Copyright: No copyright. You can use this code for anything with no warranty.
+    First, PowerShell will load the profile.ps1 file, which is the “Current User, All Hosts” profile.
+    This profile applies to all PowerShell hosts for the current user, such as the console host or the ISE host. 
+    You can use this file to define settings and commands that you want to use in any PowerShell session, regardless of the host.
+
+    Next, PowerShell will load the Microsoft.PowerShellISE_profile.ps1 file, which is the “Current User, Current Host” 
+    profile for the ISE host. This profile applies only to the PowerShell ISE host for the current user. 
+    You can use this file to define settings and commands that are specific to the ISE host, 
+    such as customizing the ISE editor or adding ISE-specific functions.
+#>
+
+# Increase history
+$MaximumHistoryCount = 10000
+
+# Produce UTF-8 by default
+
+if ( $PSVersionTable.PSVersion.Major -lt 7 ) {
+	# https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget
+	
+	$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8' # Fix Encoding for PS 5.1 https://stackoverflow.com/a/40098904
+}	
+
+#-------------------------------    Functions END     -------------------------------
+$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators") ; $TAType::Add('accelerators',$TAType)
+if (test-path alias:\cd)              { remove-item -force alias:\cd }                 # We override with cd.ps1
+if (test-path alias:\chdir)           { remove-item -force alias:\chdir }              # We override with an alias to cd.ps1
+if (test-path function:\prompt)       { remove-item -force function:\prompt }          # We override with prompt.ps1
+                Set-Alias history           	Get-History                           	-Option AllScope
+                Set-Alias kill              	killx                          			-Option AllScope
+                Set-Alias mv                	Move-Item                             	-Option AllScope
+                Set-Alias pwd               	Get-Location                          	-Option AllScope
+                Set-Alias rm                	Remove-Item                           	-Option AllScope
+                Set-Alias echo              	Write-Output                          	-Option AllScope
+                Set-Alias cls               	Clear-Host                            	-Option AllScope
+                Set-Alias copy              	Copy-Item                             	-Option AllScope
+                Set-Alias del               	Remove-Item                           	-Option AllScope
+                Set-Alias dir               	Get-Childitem                         	-Option AllScope
+                Set-Alias type              	Get-Content                           	-Option AllScope
+                Set-Alias sudo                  Elevate-Process           	            -Option AllScope
+                set-alias pastDoEdit        	find-historyAppendClipboard           	-Option AllScope
+                set-alias pastDo            	find-historyInvoke                    	-Option AllScope
+                set-alias everything        	invoke-Everything                     	-Option AllScope
+                set-alias executeThis       	invoke-FuzzyWithEverything            	-Option AllScope
+                set-alias exp-pro           	open-ProfileFolder                    	-Option AllScope
+                set-alias MyAliases         	read-aliases                          	-Option AllScope                
+                set-alias printpaths        	read-EnvPaths                         	-Option AllScope
+                set-alias uptime            	read-uptime                           	-Option AllScope
+                set-alias parameters        	get-parameters                        	-Option AllScope
+                set-alias accelerators      	([accelerators]::Get)                 	-Option AllScope
+                set-alias reboot            	exit-Nrenter                          	-Option AllScope
+                set-alias reload            	initialize-profile                    	-Option AllScope
+$profileFolder = (split-path $profile -Parent)
+Update-TypeData (join-path $profileFolder "My.Types.ps1xml")
+
+
+# Sometimes home doesn't get properly set for pre-Vista LUA-style elevated admins
+ if ($home -eq "") { remove-item -force variable:\home $home = (get-content env:\USERPROFILE) (get-psprovider 'FileSystem').Home = $home } set-content env:\HOME $home
+
+
+#loadMessage
+echo (Split-Path -leaf $MyInvocation.MyCommand.Definition)
+
+Write-Host "PSVersion: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)"
+Write-Host "PSEdition: $($PSVersionTable.PSEdition)"
+Write-Host ("Profile:   " + (Split-Path -leaf $MyInvocation.MyCommand.Definition))
+
+Write-Host "This script was invoked by: "+$($MyInvocation.Line)
+
+
+#------------------------------- Styling begin --------------------------------------					      
+#change selection to neongreen
+#https://stackoverflow.com/questions/44758698/change-powershell-psreadline-menucomplete-functions-colors
+$colors = @{
+   "Selection" = "$([char]0x1b)[38;2;0;0;0;48;2;178;255;102m"
+}
+#Set-PSReadLineOption -Colors $colors
+
+# Style default PowerShell Console
+$shell = $Host.UI.RawUI
+
+$shell.WindowTitle= "PS"
+
+$shell.BackgroundColor = "Black"
+$shell.ForegroundColor = "White"
+
+# Load custom theme for Windows Terminal
+#Set-Theme LazyAdmin
+
+
+function Ensure-Path {
+    param (
+        [string]$Path
+    )
+    # Validate the parameter
+    if (-not $Path) {
+        Write-Error "Path parameter is required"
+        return
+    }
+    # Check if the path is valid
+    if (-not [System.IO.Path]::IsPathRooted($Path)) {
+        # The path is relative, resolve it to an absolute path
+        $Path = Join-Path -Path (Get-Location) -ChildPath $Path
+    }
+    # Check if the path contains invalid characters
+    if ([System.IO.Path]::GetInvalidPathChars() -join '' -match [regex]::Escape($Path)) {
+        # The path contains invalid characters, throw an error
+        throw "The path '$Path' contains invalid characters."
+    }
+    # Check if the path exists
+    if (Test-Path -Path $Path) {
+        # The path exists, return it
+        return $Path
+    }
+    else {
+        # The path does not exist, try to create it
+        try {
+            $item = New-Item -Path $Path -ItemType Directory -Force -ErrorAction Stop
+            # Return the full path of the created directory
+            return $item.FullName
+        }
+        catch {
+            # An error occurred while creating the path, throw an error
+            throw "Failed to create the path '$Path': $($_.Exception.Message)"
+        }
+    }
+}
+function Invoke-Git {
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$Command = "status" # The git command to run
+    )
+
+    if ($Command -eq "") {
+        $Command = "status"
+    } elseif ($Command.StartsWith("git ")) {
+        $Command = $Command.Substring(4)
+    }
+
+    # Run the command and capture the output
+    $output = Invoke-Expression -Command "git $Command 2>&1" -ErrorAction Stop 
+
+    # Check the exit code and throw an exception if not zero
+    if ($LASTEXITCODE -ne 0) {
+        $errorMessage = $Error[0].Exception.Message
+        throw "Git command failed: git $Command. Error message: $errorMessage"
+    }
+
+    # return the output to the host
+    $output
+}
+function Spotify-UrlToPlaylist { $original = get-clipboard ; $transformed = $original.replace(“https://open.spotify.com/playlist/”, “spotify:user:spotify:playlist:”).replace(“?si=”, “=”) ; ($transformed -split '=')[0] | set-clipboard ; "done" }
+function git-filter-folder
+   {
+      param(
+      $namex
+      )
+      $current = git branch --show-current;
+      $branchName = ($namex+'b');
+      
+      git checkout -b $branchName
+      
+      git filter-repo --refs $branchName --subdirectory-filter $namex
+      
+      git checkout $current
+      
+      git filter-repo --refs $current --path $namex --invert-paths      
+   }
---
+++ _Helpers/EverythingHelpers.psm1
@@ -0,0 +1,116 @@
+﻿    if (Test-CommandExists 'search-Everything')
+    { 
+        function invoke-Everything([string]$filter) { @(Search-Everything  -filter $filter -global) }
+        function invoke-FuzzyWithEverything($searchstring) { menu @(invoke-Everything "ext:exe $searchString") | %{& $_ } } #use whatpulse db first, then everything #todo: sort by rescent use #use everything to find executable for fast execution
+        function Every-execute ($inputx)                { $filter = "ext:exe $inputx" ; $filter } #& (Every-Menu $filter)
+        function Every-AsHashMap                        { param( $filter = 'ext:psd1 \module')  $q = @{}                                                                                                                                                                                                                        ; everything $filter                                                                    | %{@{ name = (get -item $_).name                                  ; time=(get -item $_).LastWriteTime                            ; path=(get -item $_) } }                                                         | sort -object -property time                  | %{ $q[$_.name] = $_.path }                                                             ; $q                          | select -property values}
+        function Every-execute                          { param( $filter = 'regex:".*\\data\\[^\\]*.ahk"',$navigate=$true) Every-Menu | %                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 { if($navigate)                                                                                                                                                                                                                                                                                                      {cd ($_ | split -path -parent)} ; . $_ } }
+#        function Every-Load                             { param( $psFileFilter = 'convert-xlsx-to-csv.ps1') . ( everythnig $psFileFilter | select -first 1) } ; invoke-expression "ExcelToCsv -File 'D:\unsorted\fannyUtskick.xlsx'"
+        function Every-Explore                          { param( $filter = 'ext:exe lasso') ; Every-Menu $filter | %                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      { $path = if(!( Test-Path $_ -PathType Container))                                                                                                                                                                                                                                                                   { $_ | split-path -leaf } else                                                                                                                                                                                                                                                                                                                                                                                                                                                          {$_} ; explorer $path } }
+        function Every-Menu { param( $filter) $a= @(invoke-Everything $filter) ;  if($a.count -eq 1) {$a} else {menu $a} }
+
+<#initialy : @(Search-Everything  -filter $filter -global) #>	
+		function invoke-Everything {
+		    param(
+		        [Parameter(Mandatory=$false)]
+		        [string]$folderPath = "B:\PF\NoteTakingProjectFolder",
+		        [Parameter(Mandatory=$false)]
+		        [string]$filter = '<wholefilename:child:.git file:>|<wholefilename:child:.git folder:>',
+		        [Parameter(Mandatory=$false)]
+		        [switch]$notGlobal = $false
+		    )
+
+		    #install-module pseverything
+		    #Import-Module pseverything
+
+		    $folderPath = $folderPath.trim("\\")
+
+		    # Build a string with the parameters for Search-Everything
+		    $searchString = "Search-Everything"
+    
+		    if ($folderPath) {
+		        $searchString += " -PathInclude '$folderPath'"
+		    }
+
+		    if ($filter) {
+		        $searchString += " -Filter '$filter'"
+		    }
+
+		    if (!$notGlobal) {
+		        $searchString += " -Global"
+		    }
+		    $lines = @()
+		    # Invoke the string as a command and get the folders
+		    $lines =  Invoke-Expression $searchString
+		    return $lines
+		}          
+	
+	function everything-GitRoots ($pathx,$search)
+	{
+	       # Get the folder path from the user input
+    
+    
+
+	    # Search for files with extension .ps1 using Everything
+	    $files = Invoke-Everything -folderPath $pathx -filter $search  | %{ $_ | Get-Item }
+
+	    # Initialize a hashtable to store the git root paths and the file paths
+	    $gitRoots = @{}
+	    Write-Output ($files | Measure-Object)
+	    # Loop through each file
+	    foreach ($file in $files) {
+	        # Get the full path of the file
+	        $filePath = $file
+
+	        # Try to get the git root path of the file using git rev-parse --show-toplevel
+	        try {
+	            $filexp = $file | Split-Path -Parent
+	            $expr = "git -C '"+$filexp+"' rev-parse --show-toplevel"
+	            $gitRoot = invoke-expression $expr
+
+	            # If the git root path is not in the hashtable, create a new entry with an empty array
+	            if (-not $gitRoots.ContainsKey($gitRoot)) {
+	                $gitRoots[$gitRoot] = @()
+	            }
+
+	            # Add the file path to the array of the corresponding git root path
+	            $gitRoots[$gitRoot] += $filePath
+	        }
+	        # Catch any error and assign it to the git root variable
+	        catch {
+	            write-error $expr
+	            Write-Error $filePath
+	            $gitRoot = "error: $_"
+	        }
+
+        
+	    }
+
+	    # Loop through each git root path in the hashtable
+	    foreach ($gitRoot in $gitRoots.Keys) {
+	        # Print the git root path
+	        Write-Host "Git root: $gitRoot"
+
+	        # Print the file paths under the git root path
+	        foreach ($filePath in $gitRoots[$gitRoot]) {
+	            Write-Host "  - $filePath"
+	        }
+	    }
+	}
+
+
+	#src: https://devblogs.microsoft.com/scripting/use-a-powershell-function-to-see-if-a-command-exists/
+	function Test-CommandExists {
+	    Param ($command)
+	    $oldErrorActionPreference = $ErrorActionPreference
+	    $ErrorActionPreference = 'stop'
+	    try { Get-Command $command; return $true }
+	    catch {return $false}
+	    finally { $ErrorActionPreference=$oldErrorActionPreference }
+	}    
+
+		
+		Set-Alias -Name everything -Value invoke-everything
+	
+	
+    }
