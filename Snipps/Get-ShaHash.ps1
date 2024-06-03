0e2a35922943613dde04121f6f0dd6d4785129d8
perXautomatik <christoffer.broback@gmail.com>
2024-06-03 05:59:13 +0200

    sha-hash accepts strings as input + reminder of ast-replace

--- Snipps/Get-ShaHash.ps1
+++ Snipps/Get-ShaHash.ps1
@@ -1,25 +1,31 @@
-#function Get-ShaHash {
-	param (
+function Get-ShaHash {
+    param (
         [Parameter(ValueFromPipeline=$true)]
-		[string]$file,
+        [string]$inp,
         [ValidateSet(1, 256, 384, 512)]
-        [int]$bit = 512
-	)
+        [int]$bit = 512,
+        [ValidateSet("File", "String")]
+        [string]$Type = "File"
+    )
 
     begin {
         function Resolve-FullPath ([string]$Path) {    
-            if ( -not ([IO.Path]::IsPathRooted($Path)) ) {
+            if (-not ([IO.Path]::IsPathRooted($Path))) {
                 $Path = Join-Path $PWD $Path
             }
             [IO.Path]::GetFullPath($Path)
         }
+
+        $sha = New-Object System.Security.Cryptography.SHA$bit`CryptoServiceProvider
     }
 
     process {
-        $file = Resolve-FullPath $file
-        
-        $sha = New-Object System.Security.Cryptography.SHA$bit`CryptoServiceProvider
-        $hash = [BitConverter]::ToString($sha.ComputeHash([IO.File]::ReadAllBytes([IO.FileInfo]$file)))
-        $hash
+        if ($Type -eq "File") {
+            $inp = Resolve-FullPath $inp
+            $toHash = [IO.File]::ReadAllBytes([IO.FileInfo]$inp)
+        } else {
+            $toHash = [Text.Encoding]::UTF8.GetBytes($inp)
+        }
+        [BitConverter]::ToString($sha.ComputeHash($toHash))
     }
-#}
+}
