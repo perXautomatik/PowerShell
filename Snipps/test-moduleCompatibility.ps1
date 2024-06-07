4b085bd3fdfea66c02747abb68de1d691d719c65
perXautomatik <christoffer.broback@gmail.com>
2024-06-08 00:04:47 +0200

    some more module related tests and lists

--- Snipps/test-moduleCompatibility.ps1
+++ Snipps/test-moduleCompatibility.ps1
@@ -10,22 +10,73 @@ function Test-ModuleCompatibility {
             $requiredVersion = $m.PowerShellVersion
             # Extract the compatible PowerShell host names from the module manifest
             $compatibleHosts = $m.PowerShellHostName
-            $compatibPsEditions = $m.compatiblePsEditions 
+            # Extract the compatible PowerShell editions from the module manifest
+            $compatibleEditions = $m.CompatiblePSEditions
+
             # Check if the current host is compatible
-            $isHostCompatible = $false
-            if (($compatibleHosts -and $host.Name -in $compatibleHosts) -or !$compatibleHosts) {
+            $isHostCompatible = $false  -or !$compatibleHosts
+            if (-not $compatibleHosts -or $host.Name -in $compatibleHosts) {
                 $isHostCompatible = $true
             }
 
+            # Check if the current edition is compatible
+            $isEditionCompatible = $false   -or !$isEditionCompatible
+            if (-not $compatibleEditions -or $PSEdition -in $compatibleEditions) {
+                $isEditionCompatible = $true
+            }
+
             # Compare the required version with the current PowerShell version
-            # and check if the host is compatible
-            if ([Version]$requiredVersion -le $PSVersionTable.PSVersion -and $isHostCompatible) {
+            # and check if the host and edition are compatible
+            if ([Version]$requiredVersion -le $PSVersionTable.PSVersion -and $isHostCompatible -and $isEditionCompatible) {
                 # If compatible, output the module info
                 $m
+            }
+            else {
+                Write-Verbose "not compatible" 
+                Write-Verbose $m
             }
         }
     }
 }
 
-# Usage:
-Get-Module -ListAvailable | Test-ModuleCompatibility
+function List-Modules-OfPath {
+    ($env:PSModulePath).split(";") | %{ Get-ChildItem $_ -ErrorAction SilentlyContinue } 
+}
+function List-Modules-OfScope {
+    # Usage:
+    $modules = Get-Module -ListAvailable 
+
+    $modules += get-module
+
+    $hm = @{}
+    ($env:PSModulePath).split(";") | 
+        % { 
+                $mp = $_; 
+                $hm[$mp]=@( 
+                    $modules |
+                        ? {$_.path.StartsWith($mp, [StringComparison]::OrdinalIgnoreCase)}) 
+        };
+    $hm        
+}
+
+function List-Modules-ByFunchtion-Exposing {
+     
+    param(
+       [Parameter(ValueFromPipeline=$true)]
+       $Module
+   )
+   begin
+   {$u = @{} ; }
+   process {
+       foreach ($m in $Module) {        
+           $u[$m]+= @(Get-Module -Name $m -ListAvailable | %{ $m.ExportedCommands.Values }) 
+       }
+   }
+   end
+   {
+       $u.GetEnumerator() |
+       Group-Object -Property @{expr={ $_.Value.length}} | 
+       Sort-Object -Property name,Count |
+           select -Property Group -ExpandProperty group
+   }
+} 
