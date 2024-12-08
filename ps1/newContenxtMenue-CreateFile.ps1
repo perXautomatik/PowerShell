d9d4890dabda770a8c561d0b6083069bcc7bd2cd
Christoffer Brobäck <christoffer.broback@gmail.com>
2024-12-08 06:33:49 +0100

    state

---
+++ ps1/newContenxtMenue-CreateFile.ps1
@@ -0,0 +1,13 @@
+﻿$RegistryPath = 'HKCU:\Software\Classes\Directory\Background\Shell\NewGit'
+$Command = 'cmd.exe /c echo gitdir: > "%V\\.git"'
+
+# Create the registry key for the new context menu entry
+New-Item -Path $RegistryPath -Force
+
+# Add the command that will be executed when the menu item is clicked
+New-ItemProperty -Path $RegistryPath -Name 'Icon' -Value 'notepad.exe' -PropertyType String -Force
+New-Item -Path "$RegistryPath\command" -Force | New-ItemProperty -Name '(default)' -Value $Command -PropertyType String -Force
+
+# Refresh the shell to update the context menu
+$null = New-Object -ComObject Shell.Application
+$null.Namespace(0).Self.InvokeVerb('Refresh')
